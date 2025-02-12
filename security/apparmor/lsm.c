1de1bd07f46733da97fd1408e99534329aa42a96
Paul Moore <paul@paul-moore.com>
2025-02-12 14:45:06 -0500

    lsm: replace the name field with a pointer to the lsm_id struct
    Reduce the duplication between the lsm_id struct and the DEFINE_LSM()
    definition by linking the lsm_id struct directly into the individual
    LSM's DEFINE_LSM() instance.
    
    Linking the lsm_id into the LSM definition also allows us to simplify
    the security_add_hooks() function by removing the code which populates
    the lsm_idlist[] array and moving it into the normal LSM startup code
    where the LSM list is parsed and the individual LSMs are enabled,
    making for a cleaner implementation with less overhead at boot.
    
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -2272,7 +2272,7 @@ alloc_out:
 }
 
 DEFINE_LSM(apparmor) = {
-	.name = "apparmor",
+	.id = &apparmor_lsmid,
 	.flags = LSM_FLAG_LEGACY_MAJOR | LSM_FLAG_EXCLUSIVE,
 	.enabled = &apparmor_enabled,
 	.blobs = &apparmor_blob_sizes,
