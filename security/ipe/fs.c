524e57dc890d8e1d98445406a135480d31fcd40c
Paul Moore <paul@paul-moore.com>
2025-02-18 17:13:08 -0500

    ipe: move initcalls to the LSM framework
    Reviewed-by: Kees Cook <kees@kernel.org>
    Tested-by: Fan Wu <wufan@kernel.org>
    Acked-by: Fan Wu <wufan@kernel.org>
    Reviewed-by: John Johansen <john.johhansen@canonical.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/ipe/fs.c
+++ security/ipe/fs.c
@@ -187,7 +187,7 @@ static const struct file_operations enforce_fops = {
  * Return: %0 on success. If an error occurs, the function will return
  * the -errno.
  */
-static int __init ipe_init_securityfs(void)
+int __init ipe_init_securityfs(void)
 {
 	int rc = 0;
 	struct ipe_policy *ap;
@@ -243,5 +243,3 @@ err:
 	securityfs_remove(root);
 	return rc;
 }
-
-fs_initcall(ipe_init_securityfs);
