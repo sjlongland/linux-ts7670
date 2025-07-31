430831447d0d18e3b9e222928f09f8ccbdb4a3f5
Leo Yan <leo.yan@arm.com>
2025-07-31 13:23:46 +0100

    coresight: Refactor runtime PM
    The validation for driver data pointers and clock pointers are redundant
    in the runtime PM callbacks.  After a driver's probing, its driver data
    and clocks have been initialized successfully, this ensures it is safe
    to access driver data and clocks in the runtime PM callbacks.  A corner
    case is a clock pointer is NULL, in this case, the clock core layer can
    handle it properly.  So remove these redundant checking.
    
    In runtime resume, respect values returned from clock function and add
    error handling.
    
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Reviewed-by: Yeoreum Yun <yeoreum.yun@arm.com>
    Tested-by: James Clark <james.clark@linaro.org>
    Signed-off-by: Leo Yan <leo.yan@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Link: https://lore.kernel.org/r/20250731-arm_cs_fix_clock_v4-v6-10-1dfe10bb3f6f@arm.com

--- drivers/hwtracing/coresight/coresight-etb10.c
+++ drivers/hwtracing/coresight/coresight-etb10.c
@@ -807,8 +807,7 @@ static int etb_runtime_suspend(struct device *dev)
 {
 	struct etb_drvdata *drvdata = dev_get_drvdata(dev);
 
-	if (drvdata && !IS_ERR(drvdata->atclk))
-		clk_disable_unprepare(drvdata->atclk);
+	clk_disable_unprepare(drvdata->atclk);
 
 	return 0;
 }
@@ -817,10 +816,7 @@ static int etb_runtime_resume(struct device *dev)
 {
 	struct etb_drvdata *drvdata = dev_get_drvdata(dev);
 
-	if (drvdata && !IS_ERR(drvdata->atclk))
-		clk_prepare_enable(drvdata->atclk);
-
-	return 0;
+	return clk_prepare_enable(drvdata->atclk);
 }
 #endif
 
