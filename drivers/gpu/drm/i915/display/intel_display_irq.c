eedde2077446e858395df614b27b29ebd491486a
Jani Nikula <jani.nikula@intel.com>
2025-08-05 14:56:56 +0300

    drm/i915: silence rpm wakeref asserts on GEN11_GU_MISC_IIR access
    Commit 8d9908e8fe9c ("drm/i915/display: remove small micro-optimizations
    in irq handling") not only removed the optimizations, it also enabled
    wakeref asserts for the GEN11_GU_MISC_IIR access. Silence the asserts by
    wrapping the access inside intel_display_rpm_assert_{block,unblock}().
    
    Reported-by: "Jason A. Donenfeld" <Jason@zx2c4.com>
    Closes: https://lore.kernel.org/r/aG0tWkfmxWtxl_xc@zx2c4.com
    Fixes: 8d9908e8fe9c ("drm/i915/display: remove small micro-optimizations in irq handling")
    Cc: stable@vger.kernel.org # v6.13+
    Suggested-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Jouni Högander <jouni.hogander@intel.com>
    Link: https://lore.kernel.org/r/20250805115656.832235-1-jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_irq.c
+++ drivers/gpu/drm/i915/display/intel_display_irq.c
@@ -1525,9 +1525,13 @@ u32 gen11_gu_misc_irq_ack(struct drm_i915_private *i915, const u32 master_ctl)
 	if (!(master_ctl & GEN11_GU_MISC_IRQ))
 		return 0;
 
+	intel_display_rpm_assert_block(display);
+
 	iir = intel_de_read(display, GEN11_GU_MISC_IIR);
 	if (likely(iir))
 		intel_de_write(display, GEN11_GU_MISC_IIR, iir);
+
+	intel_display_rpm_assert_unblock(display);
 
 	return iir;
 }
