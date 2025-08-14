6f0bc690143df8b01250a1d531e20b7779d83401
André Almeida <andrealmeid@igalia.com>
2025-08-14 14:22:19 -0300

    ovl: Check for casefold consistency when creating new dentries
    In a overlayfs with casefold enabled, all new dentries should have
    casefold enabled as well. Check this at ovl_create_real().
    
    Reviewed-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: André Almeida <andrealmeid@igalia.com>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/dir.c
+++ fs/overlayfs/dir.c
@@ -162,6 +162,13 @@ struct dentry *ovl_create_real(struct ovl_fs *ofs, struct inode *dir,
 			/* mkdir is special... */
 			newdentry =  ovl_do_mkdir(ofs, dir, newdentry, attr->mode);
 			err = PTR_ERR_OR_ZERO(newdentry);
+			/* expect to inherit casefolding from workdir/upperdir */
+			if (!err && ofs->casefold != ovl_dentry_casefolded(newdentry)) {
+				pr_warn_ratelimited("wrong inherited casefold (%pd2)\n",
+						    newdentry);
+				dput(newdentry);
+				err = -EINVAL;
+			}
 			break;
 
 		case S_IFCHR:
