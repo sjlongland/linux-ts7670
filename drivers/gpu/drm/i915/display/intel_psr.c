838ca0b82c1dcb13f864cd85489de67fbcfda69a
Jouni Högander <jouni.hogander@intel.com>
2025-08-15 11:45:34 +0300

    drm/i915/psr: Check pause counter before continuing to PSR activation
    Currently intel_psr_work is re-activating PSR even when pause_counter > 0
    which is incorrect. Fix this by checking pause_counter before re-activating
    PSR.
    
    Closes: https://gitlab.freedesktop.org/drm/i915/kernel/-/issues/14822
    Signed-off-by: Jouni Högander <jouni.hogander@intel.com>
    Reviewed-by: Mika Kahola <mika.kahola@intel.com>
    Link: https://lore.kernel.org/r/20250815084534.1637030-4-jouni.hogander@intel.com

--- drivers/gpu/drm/i915/display/intel_psr.c
+++ drivers/gpu/drm/i915/display/intel_psr.c
@@ -3131,6 +3131,9 @@ static void intel_psr_work(struct work_struct *work)
 		goto unlock;
 	}
 
+	if (intel_dp->psr.pause_counter)
+		goto unlock;
+
 	/*
 	 * We have to make sure PSR is ready for re-enable
 	 * otherwise it keeps disabled until next full enable/disable cycle.
