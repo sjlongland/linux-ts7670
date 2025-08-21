29413dee04e89f48d5e6c8f03f0c802bfc6ed355
Vladimir Oltean <vladimir.oltean@nxp.com>
2025-08-21 18:20:20 +0300

    net: phy: aquantia: rename aqr113c_config_init() to aqr_gen4_config_init()
    aqr113c_config_init() is called by AQR113, AQR113C, AQR115C, all Gen4
    PHYs. Thus, rename this to aqr_gen4_config_init().
    
    Currently, aqr113c_config_init() calls aqr_gen2_config_init(). Since
    we've established that these are Gen4 PHYs, it makes sense to inherit
    the Gen3 feature set as well. Currently, aqr_gen3_config_init() just
    calls aqr_gen2_config_init(), so we can safely make this extra
    modification and expect no functional change.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Link: https://patch.msgid.link/20250821152022.1065237-14-vladimir.oltean@nxp.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/phy/aquantia/aquantia_main.c
+++ drivers/net/phy/aquantia/aquantia_main.c
@@ -1119,14 +1119,14 @@ static int aqr111_get_features(struct phy_device *phydev)
 	return 0;
 }
 
-static int aqr113c_config_init(struct phy_device *phydev)
+static int aqr_gen4_config_init(struct phy_device *phydev)
 {
 	struct aqr107_priv *priv = phydev->priv;
 	int ret;
 
 	priv->wait_on_global_cfg = true;
 
-	ret = aqr_gen2_config_init(phydev);
+	ret = aqr_gen3_config_init(phydev);
 	if (ret < 0)
 		return ret;
 
@@ -1367,7 +1367,7 @@ static struct phy_driver aqr_driver[] = {
 	.name		= "Aquantia AQR113",
 	.probe          = aqr107_probe,
 	.get_rate_matching = aqr_gen2_get_rate_matching,
-	.config_init    = aqr113c_config_init,
+	.config_init    = aqr_gen4_config_init,
 	.config_aneg    = aqr_config_aneg,
 	.config_intr    = aqr_config_intr,
 	.handle_interrupt       = aqr_handle_interrupt,
@@ -1391,7 +1391,7 @@ static struct phy_driver aqr_driver[] = {
 	.name           = "Aquantia AQR113C",
 	.probe          = aqr107_probe,
 	.get_rate_matching = aqr_gen2_get_rate_matching,
-	.config_init    = aqr113c_config_init,
+	.config_init    = aqr_gen4_config_init,
 	.config_aneg    = aqr_config_aneg,
 	.config_intr    = aqr_config_intr,
 	.handle_interrupt       = aqr_handle_interrupt,
@@ -1440,7 +1440,7 @@ static struct phy_driver aqr_driver[] = {
 	.name           = "Aquantia AQR115C",
 	.probe          = aqr107_probe,
 	.get_rate_matching = aqr_gen2_get_rate_matching,
-	.config_init    = aqr113c_config_init,
+	.config_init    = aqr_gen4_config_init,
 	.config_aneg    = aqr_config_aneg,
 	.config_intr    = aqr_config_intr,
 	.handle_interrupt = aqr_handle_interrupt,
