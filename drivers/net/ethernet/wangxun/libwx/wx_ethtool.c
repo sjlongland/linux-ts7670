3ff37b0c2c9803dbd250930d290ad27255f86e4d
Jiawen Wu <jiawenwu@trustnetic.com>
2025-08-21 10:34:05 +0800

    net: ngbe: change the default ITR setting
    Change the default RX/TX ITR for wx_mac_em devices from 20K to 7K, which
    is an experience value from out-of-tree ngbe driver, to get higher
    performance on some platforms with weak single-core performance.
    
    TCP_SRTEAM test on Phytium 2000+ shows that the throughput of 64-Byte
    packets is increased from 350.53Mbits/s to 395.92Mbits/s.
    
    Signed-off-by: Jiawen Wu <jiawenwu@trustnetic.com>
    Reviewed-by: Michal Swiatkowski <michal.swiatkowski@linux.intel.com>
    Link: https://patch.msgid.link/20250821023408.53472-2-jiawenwu@trustnetic.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/wangxun/libwx/wx_ethtool.c
+++ drivers/net/ethernet/wangxun/libwx/wx_ethtool.c
@@ -361,10 +361,14 @@ int wx_set_coalesce(struct net_device *netdev,
 	else
 		wx->rx_itr_setting = ec->rx_coalesce_usecs;
 
-	if (wx->rx_itr_setting == 1)
-		rx_itr_param = WX_20K_ITR;
-	else
+	if (wx->rx_itr_setting == 1) {
+		if (wx->mac.type == wx_mac_em)
+			rx_itr_param = WX_7K_ITR;
+		else
+			rx_itr_param = WX_20K_ITR;
+	} else {
 		rx_itr_param = wx->rx_itr_setting;
+	}
 
 	if (ec->tx_coalesce_usecs > 1)
 		wx->tx_itr_setting = ec->tx_coalesce_usecs << 2;
@@ -378,7 +382,7 @@ int wx_set_coalesce(struct net_device *netdev,
 			tx_itr_param = WX_12K_ITR;
 			break;
 		default:
-			tx_itr_param = WX_20K_ITR;
+			tx_itr_param = WX_7K_ITR;
 			break;
 		}
 	} else {
