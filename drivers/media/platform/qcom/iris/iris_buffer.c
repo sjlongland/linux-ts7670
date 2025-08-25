41f0c8da3f17552955e1ef1cbc0de4527a5b1a16
Dikshita Agarwal <quic_dikshita@quicinc.com>
2025-08-25 12:30:27 +0530

    media: iris: Fix memory leak by freeing untracked persist buffer
    One internal buffer which is allocated only once per session was not
    being freed during session close because it was not being tracked as
    part of internal buffer list which resulted in a memory leak.
    
    Add the necessary logic to explicitly free the untracked internal buffer
    during session close to ensure all allocated memory is released
    properly.
    
    Fixes: 73702f45db81 ("media: iris: allocate, initialize and queue internal buffers")
    Cc: stable@vger.kernel.org
    Reviewed-by: Vikash Garodia <quic_vgarodia@quicinc.com>
    Tested-by: Vikash Garodia <quic_vgarodia@quicinc.com> # X1E80100
    Tested-by: Neil Armstrong <neil.armstrong@linaro.org> # on SM8550-HDK
    Tested-by: Neil Armstrong <neil.armstrong@linaro.org> # on SM8650-HDK
    Signed-off-by: Dikshita Agarwal <quic_dikshita@quicinc.com>
    Tested-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org> # x1e80100-crd
    Signed-off-by: Bryan O'Donoghue <bod@kernel.org>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- drivers/media/platform/qcom/iris/iris_buffer.c
+++ drivers/media/platform/qcom/iris/iris_buffer.c
@@ -402,6 +402,16 @@ int iris_destroy_internal_buffers(struct iris_inst *inst, u32 plane)
 		}
 	}
 
+	if (force) {
+		buffers = &inst->buffers[BUF_PERSIST];
+
+		list_for_each_entry_safe(buf, next, &buffers->list, list) {
+			ret = iris_destroy_internal_buffer(inst, buf);
+			if (ret)
+				return ret;
+		}
+	}
+
 	return 0;
 }
 
