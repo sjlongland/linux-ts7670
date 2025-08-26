c2c2c18db0458b048ac300d0acb092fb77f01a53
Ping-Ke Shih <pkshih@realtek.com>
2025-08-26 16:52:58 +0800

    wifi: rtw89: pci: define TX/RX buffer descriptor pool
    Buffer descriptor (BD) is a helper of DMA for each ring. The new hardware
    design expects a continual memory across all rings, so allocate a pool
    and assign to each ring rather than allocate a buffer for a ring
    individually.
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20250826085258.28308-1-pkshih@realtek.com

--- drivers/net/wireless/realtek/rtw89/pci.h
+++ drivers/net/wireless/realtek/rtw89/pci.h
@@ -1468,6 +1468,12 @@ struct rtw89_pci_dma_ring {
 	u32 rp; /* hw idx */
 };
 
+struct rtw89_pci_dma_pool {
+	void *head;
+	dma_addr_t dma;
+	u32 size;
+};
+
 struct rtw89_pci_tx_wd_ring {
 	void *head;
 	dma_addr_t dma;
@@ -1499,6 +1505,7 @@ struct rtw89_pci_tx_ring {
 
 struct rtw89_pci_tx_rings {
 	struct rtw89_pci_tx_ring rings[RTW89_TXCH_NUM];
+	struct rtw89_pci_dma_pool bd_pool;
 };
 
 struct rtw89_pci_rx_ring {
@@ -1512,6 +1519,7 @@ struct rtw89_pci_rx_ring {
 
 struct rtw89_pci_rx_rings {
 	struct rtw89_pci_rx_ring rings[RTW89_RXCH_NUM];
+	struct rtw89_pci_dma_pool bd_pool;
 };
 
 struct rtw89_pci_isrs {
