07a8164abfa210de42b4888b9503f4a7aee89729
Max Kellermann <max.kellermann@ionos.com>
2025-08-28 10:48:20 +0200

    huge_mm.h: disallow is_huge_zero_folio(NULL)
    Calling is_huge_zero_folio(NULL) should not be legal - it makes no sense,
    and a different (theoretical) implementation may dereference the pointer.
    But currently, lacking any explicit documentation, this call is possible.
    
    But if somebody really passes NULL, the function should not return true -
    this isn't the huge zero folio after all!  However, if the
    `huge_zero_folio` hasn't been allocated yet, it's NULL, and
    is_huge_zero_folio(NULL) just happens to return true, which is a lie.
    
    This weird side effect prevented me from reproducing a kernel crash that
    occurred when the elements of a folio_batch were NULL - since
    folios_put_refs() skips huge zero folios, this sometimes causes a crash,
    but sometimes does not.  For debugging, it is better to reveal such bugs
    reliably and not hide them behind random preconditions like "has the huge
    zero folio already been created?"
    
    To improve detection of such bugs, David Hildenbrand suggested adding a
    VM_WARN_ON_ONCE().
    
    Link: https://lkml.kernel.org/r/20250828084820.570118-1-max.kellermann@ionos.com
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>
    Reviewed-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Reviewed-by: Zi Yan <ziy@nvidia.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Barry Song <baohua@kernel.org>
    Cc: Chris Li <chrisl@kernel.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Dev Jain <dev.jain@arm.com>
    Cc: Kairui Song <kasong@tencent.com>
    Cc: Kemeng Shi <shikemeng@huaweicloud.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Mariano Pache <npache@redhat.com>
    Cc: Nhat Pham <nphamcs@gmail.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -484,6 +484,8 @@ extern unsigned long huge_zero_pfn;
 
 static inline bool is_huge_zero_folio(const struct folio *folio)
 {
+	VM_WARN_ON_ONCE(!folio);
+
 	return READ_ONCE(huge_zero_folio) == folio;
 }
 
