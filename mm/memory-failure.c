251cc5c19b6af4be186e101a1cceb91125adf449
Alistair Popple <apopple@nvidia.com>
2025-09-04 08:59:26 +1000

    mm/memremap: remove unused get_dev_pagemap() parameter
    GUP no longer uses get_dev_pagemap().  As it was the only user of the
    get_dev_pagemap() pgmap caching feature it can be removed.
    
    Link: https://lkml.kernel.org/r/20250903225926.34702-2-apopple@nvidia.com
    Signed-off-by: Alistair Popple <apopple@nvidia.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Peter Xu <peterx@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -2248,7 +2248,7 @@ int memory_failure(unsigned long pfn, int flags)
 			goto unlock_mutex;
 
 		if (pfn_valid(pfn)) {
-			pgmap = get_dev_pagemap(pfn, NULL);
+			pgmap = get_dev_pagemap(pfn);
 			put_ref_page(pfn, flags);
 			if (pgmap) {
 				res = memory_failure_dev_pagemap(pfn, flags,
