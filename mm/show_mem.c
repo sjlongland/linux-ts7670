dbfa302742138344037bb1320f2ca01ab8de7d95
Yueyang Pan <pyyjason@gmail.com>
2025-09-03 04:16:14 -0700

    mm/show_mem: add trylock while printing alloc info
    In production, show_mem() can be called concurrently from two different
    entities, for example one from oom_kill_process() another from
    __alloc_pages_slowpath from another kthread.  This patch adds a spinlock
    and invokes trylock before printing out the kernel alloc info in
    show_mem().  This way two alloc info won't interleave with each other,
    which then makes parsing easier.
    
    Link: https://lkml.kernel.org/r/4ed91296e0c595d945a38458f7a8d9611b0c1e52.1756897825.git.pyyjason@gmail.com
    Signed-off-by: Yueyang Pan <pyyjason@gmail.com>
    Acked-by: Usama Arif <usamaarif642@gmail.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Acked-by: Zi Yan <ziy@nvidia.com>
    Acked-by: Suren Baghdasaryan <surenb@google.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Brendan Jackman <jackmanb@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Vishal Moola (Oracle) <vishal.moola@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/show_mem.c
+++ mm/show_mem.c
@@ -431,7 +431,9 @@ void __show_mem(unsigned int filter, nodemask_t *nodemask, int max_zone_idx)
 	printk("%lu pages hwpoisoned\n", atomic_long_read(&num_poisoned_pages));
 #endif
 #ifdef CONFIG_MEM_ALLOC_PROFILING
-	{
+	static DEFINE_SPINLOCK(mem_alloc_profiling_spinlock);
+
+	if (spin_trylock(&mem_alloc_profiling_spinlock)) {
 		struct codetag_bytes tags[10];
 		size_t i, nr;
 
@@ -458,6 +460,7 @@ void __show_mem(unsigned int filter, nodemask_t *nodemask, int max_zone_idx)
 						  ct->lineno, ct->function);
 			}
 		}
+		spin_unlock(&mem_alloc_profiling_spinlock);
 	}
 #endif
 }
