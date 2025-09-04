32a1e2ca67953437022aec84d56cdfcad4281d7b
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2025-09-04 05:21:08 +0000

    ASoC: soc-dapm: add snd_soc_dapm_to_dev()
    Because struct snd_soc_dapm_context is soc-dapm framework specific, user
    driver don't need to access its member directly, we would like to hide
    them. struct snd_soc_dapm_context will be removed from header in the
    future.
    
    Some drivers need to get dev from dapm (which will be removed).
    We need such function. Add it.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://patch.msgid.link/87cy86x06z.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/soc-dapm.h
+++ include/sound/soc-dapm.h
@@ -459,6 +459,7 @@ void snd_soc_dapm_connect_dai_link_widgets(struct snd_soc_card *card);
 int snd_soc_dapm_update_dai(struct snd_pcm_substream *substream,
 			    struct snd_pcm_hw_params *params, struct snd_soc_dai *dai);
 int snd_soc_dapm_widget_name_cmp(struct snd_soc_dapm_widget *widget, const char *s);
+struct device *snd_soc_dapm_to_dev(struct snd_soc_dapm_context *dapm);
 
 /* dapm path setup */
 int snd_soc_dapm_new_widgets(struct snd_soc_card *card);
@@ -686,7 +687,7 @@ struct snd_soc_dapm_context {
 	unsigned int idle_bias_off:1;		/* Use BIAS_OFF instead of STANDBY */
 	unsigned int suspend_bias_off:1;	/* Use BIAS_OFF in suspend if the DAPM is idle */
 
-	struct device *dev;			/* from parent - for debug */
+	struct device *dev;			/* from parent - for debug */ /* REMOVE ME */
 	struct snd_soc_component *component;	/* parent component */
 	struct snd_soc_card *card;		/* parent card */
 
