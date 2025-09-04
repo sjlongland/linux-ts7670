bb61a56f6fd60196fbbf19b99dac161bb98efa8a
Zqiang <qiang.zhang@linux.dev>
2025-09-04 19:31:33 +0800

    workqueue: Remove rcu_read_lock/unlock() in wq_watchdog_timer_fn()
    The wq_watchdog_timer_fn() is executed in the softirq context, this
    is already in the RCU read critical section, this commit therefore
    remove rcu_read_lock/unlock() in wq_watchdog_timer_fn().
    
    Signed-off-by: Zqiang <qiang.zhang@linux.dev>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -7558,8 +7558,6 @@ static void wq_watchdog_timer_fn(struct timer_list *unused)
 	if (!thresh)
 		return;
 
-	rcu_read_lock();
-
 	for_each_pool(pool, pi) {
 		unsigned long pool_ts, touched, ts;
 
@@ -7600,8 +7598,6 @@ static void wq_watchdog_timer_fn(struct timer_list *unused)
 
 
 	}
-
-	rcu_read_unlock();
 
 	if (lockup_detected)
 		show_all_workqueues();
