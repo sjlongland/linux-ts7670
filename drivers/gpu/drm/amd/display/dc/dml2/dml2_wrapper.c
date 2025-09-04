712fc089ab875ad641df3273da9ca36cec185f89
Liao Yuanhong <liaoyuanhong@vivo.com>
2025-09-04 15:10:15 +0800

    drm/amd/display: Remove redundant ternary operators
    For ternary operators in the form of "a ? true : false" or
    "a ? false : true", if 'a' itself returns a boolean result, the ternary
    operator can be omitted. Remove redundant ternary operators to clean up the
    code.
    
    Signed-off-by: Liao Yuanhong <liaoyuanhong@vivo.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dml2/dml2_wrapper.c
+++ drivers/gpu/drm/amd/display/dc/dml2/dml2_wrapper.c
@@ -708,7 +708,7 @@ static bool dml2_validate_only(struct dc_state *context)
 	if (result)
 		result = does_configuration_meet_sw_policies(dml2, &dml2->v20.scratch.cur_display_config, &dml2->v20.scratch.mode_support_info);
 
-	return (result == 1) ? true : false;
+	return result == 1;
 }
 
 static void dml2_apply_debug_options(const struct dc *dc, struct dml2_context *dml2)
