87984cc1adc9935ecbebecb1f2a6524d7a8e2ad6
Lance Yang <lance.yang@linux.dev>
2025-09-08 17:07:41 +0800

    mm: skip mlocked THPs that are underused early in deferred_split_scan()
    When we stumble over a fully-mapped mlocked THP in the deferred shrinker,
    it does not make sense to try to detect whether it is underused, because
    try_to_map_unused_to_zeropage(), called while splitting the folio, will
    not actually replace any zeroed pages by the shared zeropage.
    
    Splitting the folio in that case does not make any sense, so let's not
    even scan to check if the folio is underused.
    
    Link: https://lkml.kernel.org/r/20250908090741.61519-1-lance.yang@linux.dev
    Signed-off-by: Lance Yang <lance.yang@linux.dev>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Acked-by: Usama Arif <usamaarif642@gmail.com>
    Reviewed-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Barry Song <baohua@kernel.org>
    Cc: Dev Jain <dev.jain@arm.com>
    Cc: Lance Yang <lance.yang@linux.dev>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Mariano Pache <npache@redhat.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Cc: Kiryl Shutsemau <kirill@shutemov.name>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -4209,6 +4209,13 @@ static unsigned long deferred_split_scan(struct shrinker *shrink,
 		bool underused = false;
 
 		if (!folio_test_partially_mapped(folio)) {
+			/*
+			 * See try_to_map_unused_to_zeropage(): we cannot
+			 * optimize zero-filled pages after splitting an
+			 * mlocked folio.
+			 */
+			if (folio_test_mlocked(folio))
+				goto next;
 			underused = thp_underused(folio);
 			if (!underused)
 				goto next;
