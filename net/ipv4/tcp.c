e798487c7c3fde81252439c8163fd13dd092dbf9
Stanislav Fomichev <sdf@fomichev.me>
2025-09-10 09:24:29 -0700

    net: devmem: expose tcp_recvmsg_locked errors
    tcp_recvmsg_dmabuf can export the following errors:
    - EFAULT when linear copy fails
    - ETOOSMALL when cmsg put fails
    - ENODEV if one of the frags is readable
    - ENOMEM on xarray failures
    
    But they are all ignored and replaced by EFAULT in the caller
    (tcp_recvmsg_locked). Expose real error to the userspace to
    add more transparency on what specifically fails.
    
    In non-devmem case (skb_copy_datagram_msg) doing `if (!copied)
    copied=-EFAULT` is ok because skb_copy_datagram_msg can return only EFAULT.
    
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Reviewed-by: Mina Almasry <almasrymina@google.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Stanislav Fomichev <sdf@fomichev.me>
    Link: https://patch.msgid.link/20250910162429.4127997-1-sdf@fomichev.me
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2791,9 +2791,9 @@ found_ok_skb:
 
 				err = tcp_recvmsg_dmabuf(sk, skb, offset, msg,
 							 used);
-				if (err <= 0) {
+				if (err < 0) {
 					if (!copied)
-						copied = -EFAULT;
+						copied = err;
 
 					break;
 				}
