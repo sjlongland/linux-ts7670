cb6007d83ae57d509e8441c0d4ed60cc298bc792
Peng Fan <peng.fan@nxp.com>
2025-09-10 15:11:45 +0800

    remoteproc: imx_rproc: Introduce start/stop/detect_mode ops for imx_rproc_dcfg
    Simplify the logic in imx_rproc_start(), imx_rproc_stop() and
    imx_rproc_detect_mode(), introduce start, stop and detect_mode ops for the
    imx_rproc_dcfg structure. Allow each platform to provide its own
    implementation of start/stop/detect_mode operations, and prepare to
    eliminate the need for multiple switch-case statements.
    
    Improve code readability and maintainability by encapsulating
    platform-specific behavior.
    
    No functional changes.
    
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Link: https://lore.kernel.org/r/20250910-imx-rproc-cleanup-v2-1-10386685b8a9@nxp.com
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- drivers/remoteproc/imx_rproc.c
+++ drivers/remoteproc/imx_rproc.c
@@ -375,6 +375,11 @@ static int imx_rproc_start(struct rproc *rproc)
 	if (ret)
 		return ret;
 
+	if (dcfg->ops && dcfg->ops->start) {
+		ret = dcfg->ops->start(rproc);
+		goto start_ret;
+	}
+
 	switch (dcfg->method) {
 	case IMX_RPROC_MMIO:
 		if (priv->gpr) {
@@ -397,6 +402,7 @@ static int imx_rproc_start(struct rproc *rproc)
 		return -EOPNOTSUPP;
 	}
 
+start_ret:
 	if (ret)
 		dev_err(dev, "Failed to enable remote core!\n");
 
@@ -410,6 +416,11 @@ static int imx_rproc_stop(struct rproc *rproc)
 	struct device *dev = priv->dev;
 	struct arm_smccc_res res;
 	int ret;
+
+	if (dcfg->ops && dcfg->ops->stop) {
+		ret = dcfg->ops->stop(rproc);
+		goto stop_ret;
+	}
 
 	switch (dcfg->method) {
 	case IMX_RPROC_MMIO:
@@ -439,6 +450,7 @@ static int imx_rproc_stop(struct rproc *rproc)
 		return -EOPNOTSUPP;
 	}
 
+stop_ret:
 	if (ret)
 		dev_err(dev, "Failed to stop remote core\n");
 	else
@@ -916,6 +928,9 @@ static int imx_rproc_detect_mode(struct imx_rproc *priv)
 	int ret;
 	u32 val;
 	u8 pt;
+
+	if (dcfg->ops && dcfg->ops->detect_mode)
+		return dcfg->ops->detect_mode(priv->rproc);
 
 	switch (dcfg->method) {
 	case IMX_RPROC_NONE:
