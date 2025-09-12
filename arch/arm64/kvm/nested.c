913c62ba7a30edc07414e166c886fd0e164da475
Oliver Upton <oliver.upton@linux.dev>
2025-09-12 14:22:57 -0700

    KVM: arm64: nv: Advertise FEAT_TIDCP1 to NV-enabled VMs
    While KVM does not expose IMPDEF features to VMs, FEAT_TIDCP1 is an
    architecturally-defined EL1 trap of a particular sysreg encoding range.
    Furthermore, KVM already advertises this feature to non-NV VMs.
    
    As there is no interaction with EL2 traps, expose the feature.
    
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/nested.c
+++ arch/arm64/kvm/nested.c
@@ -902,7 +902,6 @@ u64 limit_nv_id_reg(struct kvm *kvm, u32 reg, u64 val)
 
 	case SYS_ID_AA64MMFR1_EL1:
 		val &= ~(ID_AA64MMFR1_EL1_CMOW		|
-			 ID_AA64MMFR1_EL1_TIDCP1	|
 			 ID_AA64MMFR1_EL1_nTLBPA	|
 			 ID_AA64MMFR1_EL1_ETS		|
 			 ID_AA64MMFR1_EL1_XNX		|
