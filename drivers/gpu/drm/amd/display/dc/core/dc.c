7b6295e6ce8a2f20ee4768a374bd830575b57158
Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
2025-09-16 13:57:17 -0400

    drm/amd/display: Support possibly NULL link for should_use_dmub_lock
    [Why]
    It's possible to have a stream enabled without a link or link encoder.
    
    There are cases where we'd still like to interlock the driver
    programming from firmware programming to ensure that we don't put the
    hardware in an undefined (or error) state if two programming sequences
    are simultaneously executed on the same hardware blocks.
    
    [How]
    Add an explicit DC parameter to should_use_dmub_lock().
    
    Make pointers to should_use_dmub_lock() const since it's a checker
    function that shouldn't modify state.
    
    Update the callsites to pass in DC explicitly.
    
    Check that the link is non-NULL before deferencing and performing link
    based checks.
    
    Reviewed-by: Alvin Lee <alvin.lee2@amd.com>
    Signed-off-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Roman Li <roman.li@amd.com>
    Tested-by: Dan Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc.c
+++ drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -4058,7 +4058,7 @@ static void commit_planes_for_stream(struct dc *dc,
 	if ((update_type != UPDATE_TYPE_FAST) && stream->update_flags.bits.dsc_changed)
 		if (top_pipe_to_program &&
 			top_pipe_to_program->stream_res.tg->funcs->lock_doublebuffer_enable) {
-			if (should_use_dmub_lock(stream->link)) {
+			if (should_use_dmub_lock(dc, stream->link)) {
 				union dmub_hw_lock_flags hw_locks = { 0 };
 				struct dmub_hw_lock_inst_flags inst_flags = { 0 };
 
@@ -4302,7 +4302,7 @@ static void commit_planes_for_stream(struct dc *dc,
 				top_pipe_to_program->stream_res.tg,
 				CRTC_STATE_VACTIVE);
 
-			if (should_use_dmub_lock(stream->link)) {
+			if (should_use_dmub_lock(dc, stream->link)) {
 				union dmub_hw_lock_flags hw_locks = { 0 };
 				struct dmub_hw_lock_inst_flags inst_flags = { 0 };
 
