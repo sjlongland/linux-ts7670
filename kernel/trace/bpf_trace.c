99d3924d3e8f5c3716d73bef51d4fbcfa5eb769c
Jiri Olsa <jolsa@kernel.org>
2025-09-16 23:52:56 +0200

    bpf: Allow uprobe program to change context registers
    Currently uprobe (BPF_PROG_TYPE_KPROBE) program can't write to the
    context registers data. While this makes sense for kprobe attachments,
    for uprobe attachment it might make sense to be able to change user
    space registers to alter application execution.
    
    Since uprobe and kprobe programs share the same type (BPF_PROG_TYPE_KPROBE),
    we can't deny write access to context during the program load. We need
    to check on it during program attachment to see if it's going to be
    kprobe or uprobe.
    
    Storing the program's write attempt to context and checking on it
    during the attachment.
    
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Link: https://lore.kernel.org/r/20250916215301.664963-2-jolsa@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -1627,8 +1627,6 @@ static bool kprobe_prog_is_valid_access(int off, int size, enum bpf_access_type 
 {
 	if (off < 0 || off >= sizeof(struct pt_regs))
 		return false;
-	if (type != BPF_READ)
-		return false;
 	if (off % size != 0)
 		return false;
 	/*
@@ -1637,6 +1635,9 @@ static bool kprobe_prog_is_valid_access(int off, int size, enum bpf_access_type 
 	 */
 	if (off + size > sizeof(struct pt_regs))
 		return false;
+
+	if (type == BPF_WRITE)
+		prog->aux->kprobe_write_ctx = true;
 
 	return true;
 }
@@ -2993,6 +2994,10 @@ int bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *pr
 		return -EOPNOTSUPP;
 
 	if (!is_kprobe_multi(prog))
+		return -EINVAL;
+
+	/* Writing to context is not allowed for kprobes. */
+	if (prog->aux->kprobe_write_ctx)
 		return -EINVAL;
 
 	flags = attr->link_create.kprobe_multi.flags;
