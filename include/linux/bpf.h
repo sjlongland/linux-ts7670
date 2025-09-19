e4173d5b5f4abdffe05b48862809fa8026af5eea
Leon Hwang <leon.hwang@linux.dev>
2025-09-19 12:41:09 +0800

    bpf: Allow union argument in trampoline based programs
    Currently, functions with 'union' arguments cannot be traced with
    fentry/fexit:
    
    bpftrace -e 'fentry:release_pages { exit(); }' -v
    
    The function release_pages arg0 type UNION is unsupported.
    
    The type of the 'release_pages' arg0 is defined as:
    
    typedef union {
    	struct page **pages;
    	struct folio **folios;
    	struct encoded_page **encoded_pages;
    } release_pages_arg __attribute__ ((__transparent_union__));
    
    This patch relaxes the restriction by allowing function arguments of type
    'union' to be traced in verifier.
    
    Reviewed-by: Amery Hung <ameryhung@gmail.com>
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>
    Link: https://lore.kernel.org/r/20250919044110.23729-2-leon.hwang@linux.dev
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1094,7 +1094,7 @@ enum bpf_cgroup_storage_type {
  */
 #define MAX_BPF_FUNC_REG_ARGS 5
 
-/* The argument is a structure. */
+/* The argument is a structure or a union. */
 #define BTF_FMODEL_STRUCT_ARG		BIT(0)
 
 /* The argument is signed. */
