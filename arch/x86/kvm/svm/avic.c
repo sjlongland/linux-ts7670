ffc7d42e39dfe4fcfb5edfe0e12b335957e5cb14
Sean Christopherson <seanjc@google.com>
2025-09-19 14:59:32 -0700

    KVM: SVM: Don't advise the user to do force_avic=y (when x2AVIC is detected)
    Don't advise the end user to try to force enable AVIC when x2AVIC is
    reported as supported in CPUID, as forcefully enabling AVIC isn't something
    that should be done lightly.  E.g. some Zen4 client systems hide AVIC but
    leave x2AVIC behind, and while such a configuration is indeed due to buggy
    firmware in the sense the reporting x2AVIC without AVIC is nonsensical,
    KVM has no idea _why_ firmware disabled AVIC in the first place.
    
    Suggesting that the user try to run with force_avic=y is sketchy even if
    the user explicitly tries to enable AVIC, and will be downright
    irresponsible once KVM starts enabling AVIC by default.  Alternatively,
    KVM could print the message only when the user explicitly asks for AVIC,
    but running with force_avic=y isn't something that should be encouraged
    for random users.  force_avic is a useful knob for developers and perhaps
    even advanced users, but isn't something that KVM should advertise broadly.
    
    Opportunistically append a newline to the pr_warn() so that it prints out
    immediately, and tweak the message to say that AVIC is unsupported instead
    of disabled (disabled suggests that the kernel/KVM is somehow responsible).
    
    Suggested-by: Naveen N Rao (AMD) <naveen@kernel.org>
    Reviewed-by: Naveen N Rao (AMD) <naveen@kernel.org>
    Tested-by: Naveen N Rao (AMD) <naveen@kernel.org>
    Link: https://lore.kernel.org/r/20250919215934.1590410-6-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/svm/avic.c
+++ arch/x86/kvm/svm/avic.c
@@ -1192,10 +1192,8 @@ bool avic_hardware_setup(void)
 
 	/* AVIC is a prerequisite for x2AVIC. */
 	if (!boot_cpu_has(X86_FEATURE_AVIC) && !force_avic) {
-		if (boot_cpu_has(X86_FEATURE_X2AVIC)) {
-			pr_warn(FW_BUG "Cannot support x2AVIC due to AVIC is disabled");
-			pr_warn(FW_BUG "Try enable AVIC using force_avic option");
-		}
+		if (boot_cpu_has(X86_FEATURE_X2AVIC))
+			pr_warn(FW_BUG "Cannot enable x2AVIC, AVIC is unsupported\n");
 		return false;
 	}
 
