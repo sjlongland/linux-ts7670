2d640bdc3a28ba538f7e9572597312d3e8e5d9fd
Sean Christopherson <seanjc@google.com>
2025-09-18 17:32:59 -0700

    KVM: s390/vfio-ap: Use kvm_is_gpa_in_memslot() instead of open coded equivalent
    Use kvm_is_gpa_in_memslot() to check the validity of the notification
    indicator byte address instead of open coding equivalent logic in the VFIO
    AP driver.
    
    Opportunistically use a dedicated wrapper that exists and is exported
    expressly for the VFIO AP module.  kvm_is_gpa_in_memslot() is generally
    unsuitable for use outside of KVM; other drivers typically shouldn't rely
    on KVM's memslots, and using the API requires kvm->srcu (or slots_lock) to
    be held for the entire duration of the usage, e.g. to avoid TOCTOU bugs.
    handle_pqap() is a bit of a special case, as it's explicitly invoked from
    KVM with kvm->srcu already held, and the VFIO AP driver is in many ways an
    extension of KVM that happens to live in a separate module.
    
    Providing a dedicated API for the VFIO AP driver will allow restricting
    the vast majority of generic KVM's exports to KVM submodules (e.g. to x86's
    kvm-{amd,intel}.ko vendor mdoules).
    
    No functional change intended.
    
    Acked-by: Anthony Krowiak <akrowiak@linux.ibm.com>
    Reviewed-by: Christian Borntraeger <borntraeger@linux.ibm.com>
    Link: https://lore.kernel.org/r/20250919003303.1355064-2-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/s390/include/asm/kvm_host.h
+++ arch/s390/include/asm/kvm_host.h
@@ -719,6 +719,8 @@ bool kvm_s390_pv_cpu_is_protected(struct kvm_vcpu *vcpu);
 extern int kvm_s390_gisc_register(struct kvm *kvm, u32 gisc);
 extern int kvm_s390_gisc_unregister(struct kvm *kvm, u32 gisc);
 
+bool kvm_s390_is_gpa_in_memslot(struct kvm *kvm, gpa_t gpa);
+
 static inline void kvm_arch_free_memslot(struct kvm *kvm,
 					 struct kvm_memory_slot *slot) {}
 static inline void kvm_arch_memslots_updated(struct kvm *kvm, u64 gen) {}
