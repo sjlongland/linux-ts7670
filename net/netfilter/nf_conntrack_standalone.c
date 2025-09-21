5c82b638b41afd1cd78958ee1fe5bfb48a37253a
Florian Westphal <fw@strlen.de>
2025-09-21 17:45:30 +0200

    netfilter: conntrack: disable 0 value for conntrack_max setting
    Undocumented historical artifact inherited from ip_conntrack.
    If value is 0, then no limit is applied at all, conntrack table
    can grow to huge value, only limited by size of conntrack hashes and
    the kernel-internal upper limit on the hash chain lengths.
    
    This feature makes no sense; users can just set
    conntrack_max=2147483647 (INT_MAX).
    
    Disallow a 0 value.  This will make it slightly easier to allow
    per-netns constraints for this value in a future patch.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- net/netfilter/nf_conntrack_standalone.c
+++ net/netfilter/nf_conntrack_standalone.c
@@ -622,7 +622,7 @@ static struct ctl_table nf_ct_sysctl_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= SYSCTL_ZERO,
+		.extra1		= SYSCTL_ONE,
 		.extra2		= SYSCTL_INT_MAX,
 	},
 	[NF_SYSCTL_CT_COUNT] = {
@@ -955,7 +955,7 @@ static struct ctl_table nf_ct_netfilter_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= SYSCTL_ZERO,
+		.extra1		= SYSCTL_ONE,
 		.extra2		= SYSCTL_INT_MAX,
 	},
 };
