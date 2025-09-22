7b1d8b3944ddc7cc217a56a3d5eba6a98f0e86e8
Johan Hovold <johan@kernel.org>
2025-09-22 17:31:03 +0200

    mtd: rawnand: omap2: drop unused module alias
    The driver only supports OF probing since commit 086c321ec57b ("mtd:
    nand: omap2: Remove omap_nand_platform_data") so drop the unused
    platform module alias.
    
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/nand/raw/omap2.c
+++ drivers/mtd/nand/raw/omap2.c
@@ -2332,6 +2332,5 @@ static struct platform_driver omap_nand_driver = {
 
 module_platform_driver(omap_nand_driver);
 
-MODULE_ALIAS("platform:" DRIVER_NAME);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Glue layer for NAND flash on TI OMAP boards");
