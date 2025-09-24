3721b458e59df51601dccb2c49ca13e49aab262f
Peter Wang <peter.wang@mediatek.com>
2025-09-24 17:43:27 +0800

    scsi: ufs: host: mediatek: Fix shutdown/suspend race condition
    Address a race condition between shutdown and suspend operations in the
    UFS Mediatek driver. Before entering suspend, check if a shutdown is in
    progress to prevent conflicts and ensure system stability.
    
    Signed-off-by: Peter Wang <peter.wang@mediatek.com>
    Acked-by: Chun-Hung Wu <chun-hung.wu@mediatek.com>
    Link: https://patch.msgid.link/20250924094527.2992256-6-peter.wang@mediatek.com
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/ufs/host/ufs-mediatek.c
+++ drivers/ufs/host/ufs-mediatek.c
@@ -2141,6 +2141,11 @@ static int ufs_mtk_system_suspend(struct device *dev)
 	struct arm_smccc_res res;
 	int ret;
 
+	if (hba->shutting_down) {
+		ret = -EBUSY;
+		goto out;
+	}
+
 	ret = ufshcd_system_suspend(dev);
 	if (ret)
 		goto out;
