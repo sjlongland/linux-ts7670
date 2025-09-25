44f54fdd48eab44ee9d7c4102191f3add7932427
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-09-25 12:32:41 +0200

    mptcp: pm: in-kernel: rename 'subflows_max' to 'limit_extra_subflows'
    A few variables linked to the in-kernel Path-Manager are confusing, and
    it would help current and future developers, to clarify them.
    
    One of them is 'subflows_max', which in fact represents the limit of
    extra subflows: the limit set via 'ip mptcp limit subflows X' for
    example. It is not linked to the maximum number of created / possible
    subflows.
    
    While at it, add an additional name for the corresponding variable in
    MPTCP INFO: mptcpi_limit_extra_subflows. Not to break the current uAPI,
    the new name is added as a 'define' pointing to the former name. This
    will then also help userspace devs.
    
    No functional changes intended.
    
    Reviewed-by: Geliang Tang <geliang@kernel.org>
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Link: https://patch.msgid.link/20250925-net-next-mptcp-c-flag-laminar-v1-6-ad126cc47c6b@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/uapi/linux/mptcp.h
+++ include/uapi/linux/mptcp.h
@@ -43,6 +43,7 @@ struct mptcp_info {
 	__u8	mptcpi_add_addr_signal;
 	__u8	mptcpi_add_addr_accepted;
 	__u8	mptcpi_subflows_max;
+	#define mptcpi_limit_extra_subflows mptcpi_subflows_max
 	__u8	mptcpi_add_addr_signal_max;
 	__u8	mptcpi_add_addr_accepted_max;
 	__u32	mptcpi_flags;
