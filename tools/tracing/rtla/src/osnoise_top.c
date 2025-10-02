ebd4d6a85ca98f84b9ee60c01c8e7a7d9e3fcaf1
Costa Shulyupin <costa.shul@redhat.com>
2025-10-02 15:35:39 +0300

    tools/rtla: Remove unused optional option_index
    The longindex argument of getopt_long() is optional
    and tied to the unused local variable option_index.
    
    Remove it to shorten the four longest functions
    and make the code neater.
    
    Signed-off-by: Costa Shulyupin <costa.shul@redhat.com>
    Reviewed-by: Tomas Glozar <tglozar@redhat.com>
    Link: https://lore.kernel.org/r/20251002123553.389467-2-costa.shul@redhat.com
    Signed-off-by: Tomas Glozar <tglozar@redhat.com>

--- tools/tracing/rtla/src/osnoise_top.c
+++ tools/tracing/rtla/src/osnoise_top.c
@@ -364,11 +364,8 @@ struct osnoise_params *osnoise_top_parse_args(int argc, char **argv)
 			{0, 0, 0, 0}
 		};
 
-		/* getopt_long stores the option index here. */
-		int option_index = 0;
-
 		c = getopt_long(argc, argv, "a:c:C::d:De:hH:p:P:qr:s:S:t::T:0:1:2:3:",
-				 long_options, &option_index);
+				 long_options, NULL);
 
 		/* Detect the end of the options. */
 		if (c == -1)
