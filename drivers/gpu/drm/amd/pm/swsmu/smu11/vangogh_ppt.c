99e617cc9a6a2e2f23cb2b5d88440b17888d820c
Mario Limonciello <mario.limonciello@amd.com>
2025-10-06 11:16:20 -0500

    drm/amd: Adjust whitespace for vangogh_ppt
    A few changes have more whitespace than needed.  Clean them up.
    
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Tested-by: Robert Beckett <bob.beckett@collabora.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/smu11/vangogh_ppt.c
+++ drivers/gpu/drm/amd/pm/swsmu/smu11/vangogh_ppt.c
@@ -2315,8 +2315,7 @@ static int vangogh_get_power_limit(struct smu_context *smu,
 				   uint32_t *max_power_limit,
 				   uint32_t *min_power_limit)
 {
-	struct smu_11_5_power_context *power_context =
-								smu->smu_power.power_context;
+	struct smu_11_5_power_context *power_context = smu->smu_power.power_context;
 	uint32_t ppt_limit;
 	int ret = 0;
 
@@ -2352,12 +2351,11 @@ static int vangogh_get_power_limit(struct smu_context *smu,
 }
 
 static int vangogh_get_ppt_limit(struct smu_context *smu,
-								uint32_t *ppt_limit,
-								enum smu_ppt_limit_type type,
-								enum smu_ppt_limit_level level)
+				 uint32_t *ppt_limit,
+				 enum smu_ppt_limit_type type,
+				 enum smu_ppt_limit_level level)
 {
-	struct smu_11_5_power_context *power_context =
-							smu->smu_power.power_context;
+	struct smu_11_5_power_context *power_context = smu->smu_power.power_context;
 
 	if (!power_context)
 		return -EOPNOTSUPP;
