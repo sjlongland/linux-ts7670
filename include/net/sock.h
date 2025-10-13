0c95e0ebc33c1dbeead9c3e7b906280a59e66ce7
Eric Dumazet <edumazet@google.com>
2025-10-13 15:22:31 +0000

    net: add SK_WMEM_ALLOC_BIAS constant
    sk->sk_wmem_alloc is initialized to 1, and sk_wmem_alloc_get()
    takes care of this initial value.
    
    Add SK_WMEM_ALLOC_BIAS define to not spread this magic value.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Neal Cardwell <ncardwell@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Link: https://patch.msgid.link/20251013152234.842065-2-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2263,6 +2263,7 @@ static inline int skb_copy_to_page_nocache(struct sock *sk, struct iov_iter *fro
 	return 0;
 }
 
+#define SK_WMEM_ALLOC_BIAS 1
 /**
  * sk_wmem_alloc_get - returns write allocations
  * @sk: socket
@@ -2271,7 +2272,7 @@ static inline int skb_copy_to_page_nocache(struct sock *sk, struct iov_iter *fro
  */
 static inline int sk_wmem_alloc_get(const struct sock *sk)
 {
-	return refcount_read(&sk->sk_wmem_alloc) - 1;
+	return refcount_read(&sk->sk_wmem_alloc) - SK_WMEM_ALLOC_BIAS;
 }
 
 /**
