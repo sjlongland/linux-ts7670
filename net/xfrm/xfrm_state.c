a4a4d80bf0cfc19c4c75120bc665d6b4502e1efe
Sabrina Dubroca <sd@queasysnail.net>
2025-10-16 12:39:14 +0200

    xfrm: make state as DEAD before final put when migrate fails
    xfrm_state_migrate/xfrm_state_clone_and_setup create a new state, and
    call xfrm_state_put to destroy it in case of
    failure. __xfrm_state_destroy expects the state to be in
    XFRM_STATE_DEAD, but we currently don't do that.
    
    Reported-by: syzbot+5cd6299ede4d4f70987b@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=5cd6299ede4d4f70987b
    Fixes: 78347c8c6b2d ("xfrm: Fix xfrm_state_migrate leak")
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -2061,6 +2061,7 @@ static struct xfrm_state *xfrm_state_clone(struct xfrm_state *orig,
 	return x;
 
  error:
+	x->km.state = XFRM_STATE_DEAD;
 	xfrm_state_put(x);
 out:
 	return NULL;
@@ -2148,6 +2149,7 @@ struct xfrm_state *xfrm_state_migrate(struct xfrm_state *x,
 
 	return xc;
 error:
+	xc->km.state = XFRM_STATE_DEAD;
 	xfrm_state_put(xc);
 	return NULL;
 }
