a5bbe31ab990cdfb19249efa63fb5ee9cacd4971
Bart Van Assche <bvanassche@acm.org>
2025-10-31 13:39:29 -0700

    scsi: ufs: core: Make the reserved slot a reserved request
    Instead of letting the SCSI core allocate hba->nutrs - 1 commands, let
    the SCSI core allocate hba->nutrs commands, set the number of reserved
    tags to 1 and use the reserved tag for device management commands. This
    patch changes the 'reserved slot' from hba->nutrs - 1 into 0 because the
    block layer reserves the smallest tags for reserved commands.
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Link: https://patch.msgid.link/20251031204029.2883185-22-bvanassche@acm.org
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -2420,7 +2420,7 @@ static inline int ufshcd_hba_capabilities(struct ufs_hba *hba)
 	hba->nutrs = (hba->capabilities & MASK_TRANSFER_REQUESTS_SLOTS_SDB) + 1;
 	hba->nutmrs =
 	((hba->capabilities & MASK_TASK_MANAGEMENT_REQUEST_SLOTS) >> 16) + 1;
-	hba->reserved_slot = hba->nutrs - 1;
+	hba->reserved_slot = 0;
 
 	hba->nortt = FIELD_GET(MASK_NUMBER_OUTSTANDING_RTT, hba->capabilities) + 1;
 
@@ -8707,7 +8707,6 @@ static int ufshcd_alloc_mcq(struct ufs_hba *hba)
 		goto err;
 
 	hba->host->can_queue = hba->nutrs - UFSHCD_NUM_RESERVED;
-	hba->reserved_slot = hba->nutrs - UFSHCD_NUM_RESERVED;
 
 	return 0;
 err:
@@ -8950,6 +8949,7 @@ static const struct scsi_host_template ufshcd_driver_template = {
 	.proc_name		= UFSHCD,
 	.map_queues		= ufshcd_map_queues,
 	.queuecommand		= ufshcd_queuecommand,
+	.nr_reserved_cmds	= UFSHCD_NUM_RESERVED,
 	.mq_poll		= ufshcd_poll,
 	.sdev_init		= ufshcd_sdev_init,
 	.sdev_configure		= ufshcd_sdev_configure,
