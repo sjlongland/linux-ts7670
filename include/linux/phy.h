091ac9245d30bbee2998f0f12c486e24bd3360fb
Heiner Kallweit <hkallweit1@gmail.com>
2025-11-03 23:26:49 +0100

    net: phy: make phy_device members pause and asym_pause bitfield bits
    We can reduce the size of struct phy_device a little by switching
    the type of members pause and asym_pause from int to a single bit.
    As C99 is supported now, we can use type bool for the bitfield members,
    what provides us with the benefit of the usual implicit bool conversions.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Link: https://patch.msgid.link/764e9a31-b40b-4dc9-b808-118192a16d87@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -644,6 +644,8 @@ struct phy_device {
 	/* The most recently read link state */
 	unsigned link:1;
 	unsigned autoneg_complete:1;
+	bool pause:1;
+	bool asym_pause:1;
 
 	/* Interrupts are enabled */
 	unsigned interrupts:1;
@@ -668,8 +670,6 @@ struct phy_device {
 	int speed;
 	int duplex;
 	int port;
-	int pause;
-	int asym_pause;
 	u8 master_slave_get;
 	u8 master_slave_set;
 	u8 master_slave_state;
