d73267a76bd60d0b562beb594725f2bd2836487d
Johan Hovold <johan@kernel.org>
2025-11-10 12:12:06 +0100

    USB: serial: ftdi_sio: silence jtag probe
    Probe of a device should generally be silent unless errors are
    encountered.
    
    Stop logging that the JTAG port is ignored when probing devices with
    such a reserved port.
    
    This also maintains consistency with devices that match on interface
    number to avoid binding to reserved ports.
    
    Note that the message is not even correct for the second port of the ST
    Micro Connect Lite.
    
    Signed-off-by: Johan Hovold <johan@kernel.org>

--- drivers/usb/serial/ftdi_sio.c
+++ drivers/usb/serial/ftdi_sio.c
@@ -2309,10 +2309,8 @@ static int ftdi_jtag_probe(struct usb_serial *serial)
 	struct usb_interface *intf = serial->interface;
 	int ifnum = intf->cur_altsetting->desc.bInterfaceNumber;
 
-	if (ifnum == 0) {
-		dev_info(&intf->dev, "Ignoring interface reserved for JTAG\n");
+	if (ifnum == 0)
 		return -ENODEV;
-	}
 
 	return 0;
 }
@@ -2345,10 +2343,8 @@ static int ftdi_stmclite_probe(struct usb_serial *serial)
 	struct usb_interface *intf = serial->interface;
 	int ifnum = intf->cur_altsetting->desc.bInterfaceNumber;
 
-	if (ifnum < 2) {
-		dev_info(&intf->dev, "Ignoring interface reserved for JTAG\n");
+	if (ifnum < 2)
 		return -ENODEV;
-	}
 
 	return 0;
 }
