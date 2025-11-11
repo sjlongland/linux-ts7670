1d5976cdb10344c76a3e3f274a6b6dab555aa56d
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2025-11-11 00:29:24 +0000

    ASoC: codecs: wm8962: convert to snd_soc_dapm_xxx()
    This patch converts below functions.
    
    dapm->dev					-> snd_soc_dapm_to_dev()
    dapm->card					-> snd_soc_dapm_to_card()
    dapm->component					-> snd_soc_dapm_to_component()
    
    dapm_kcontrol_get_value()			-> snd_soc_dapm_kcontrol_get_value()
    
    snd_soc_component_enable_pin()			-> snd_soc_dapm_enable_pin()
    snd_soc_component_enable_pin_unlocked()		-> snd_soc_dapm_enable_pin_unlocked()
    snd_soc_component_disable_pin()			-> snd_soc_dapm_disable_pin()
    snd_soc_component_disable_pin_unlocked()	-> snd_soc_dapm_disable_pin_unlocked()
    snd_soc_component_nc_pin()			-> snd_soc_dapm_nc_pin()
    snd_soc_component_nc_pin_unlocked()		-> snd_soc_dapm_nc_pin_unlocked()
    snd_soc_component_get_pin_status()		-> snd_soc_dapm_get_pin_status()
    snd_soc_component_force_enable_pin()		-> snd_soc_dapm_force_enable_pin()
    snd_soc_component_force_enable_pin_unlocked()	-> snd_soc_dapm_force_enable_pin_unlocked()
    snd_soc_component_force_bias_level()		-> snd_soc_dapm_force_bias_level()
    snd_soc_component_get_bias_level()		-> snd_soc_dapm_get_bias_level()
    snd_soc_component_init_bias_level()		-> snd_soc_dapm_init_bias_level()
    snd_soc_component_get_dapm()			-> snd_soc_component_to_dapm()
    
    snd_soc_dapm_kcontrol_component()		-> snd_soc_dapm_kcontrol_to_component()
    snd_soc_dapm_kcontrol_widget()			-> snd_soc_dapm_kcontrol_to_widget()
    snd_soc_dapm_kcontrol_dapm()			-> snd_soc_dapm_kcontrol_to_dapm()
    snd_soc_dapm_np_pin()				-> snd_soc_dapm_disable_pin()
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Reviewed-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://patch.msgid.link/87cy5pza1n.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/wm8962.c
+++ sound/soc/codecs/wm8962.c
@@ -2418,7 +2418,7 @@ static int wm8962_add_widgets(struct snd_soc_component *component)
 {
 	struct wm8962_priv *wm8962 = snd_soc_component_get_drvdata(component);
 	struct wm8962_pdata *pdata = &wm8962->pdata;
-	struct snd_soc_dapm_context *dapm = snd_soc_component_get_dapm(component);
+	struct snd_soc_dapm_context *dapm = snd_soc_component_to_dapm(component);
 
 	snd_soc_add_component_controls(component, wm8962_snd_controls,
 			     ARRAY_SIZE(wm8962_snd_controls));
@@ -2465,6 +2465,7 @@ static const int sysclk_rates[] = {
 
 static void wm8962_configure_bclk(struct snd_soc_component *component)
 {
+	struct snd_soc_dapm_context *dapm = snd_soc_component_to_dapm(component);
 	struct wm8962_priv *wm8962 = snd_soc_component_get_drvdata(component);
 	int best, min_diff, diff;
 	int dspclk, i;
@@ -2504,7 +2505,7 @@ static void wm8962_configure_bclk(struct snd_soc_component *component)
 	 * So we here provisionally enable it and then disable it afterward
 	 * if current bias_level hasn't reached SND_SOC_BIAS_ON.
 	 */
-	if (snd_soc_component_get_bias_level(component) != SND_SOC_BIAS_ON)
+	if (snd_soc_dapm_get_bias_level(dapm) != SND_SOC_BIAS_ON)
 		snd_soc_component_update_bits(component, WM8962_CLOCKING2,
 				WM8962_SYSCLK_ENA_MASK, WM8962_SYSCLK_ENA);
 
@@ -2518,7 +2519,7 @@ static void wm8962_configure_bclk(struct snd_soc_component *component)
 	usleep_range(500, 1000);
 	dspclk = snd_soc_component_read(component, WM8962_CLOCKING1);
 
-	if (snd_soc_component_get_bias_level(component) != SND_SOC_BIAS_ON)
+	if (snd_soc_dapm_get_bias_level(dapm) != SND_SOC_BIAS_ON)
 		snd_soc_component_update_bits(component, WM8962_CLOCKING2,
 				WM8962_SYSCLK_ENA_MASK, 0);
 
@@ -2578,6 +2579,8 @@ static void wm8962_configure_bclk(struct snd_soc_component *component)
 static int wm8962_set_bias_level(struct snd_soc_component *component,
 				 enum snd_soc_bias_level level)
 {
+	struct snd_soc_dapm_context *dapm = snd_soc_component_to_dapm(component);
+
 	switch (level) {
 	case SND_SOC_BIAS_ON:
 		break;
@@ -2595,7 +2598,7 @@ static int wm8962_set_bias_level(struct snd_soc_component *component,
 		snd_soc_component_update_bits(component, WM8962_PWR_MGMT_1,
 				    WM8962_VMID_SEL_MASK, 0x100);
 
-		if (snd_soc_component_get_bias_level(component) == SND_SOC_BIAS_OFF)
+		if (snd_soc_dapm_get_bias_level(dapm) == SND_SOC_BIAS_OFF)
 			msleep(100);
 		break;
 
@@ -2628,6 +2631,7 @@ static int wm8962_hw_params(struct snd_pcm_substream *substream,
 			    struct snd_soc_dai *dai)
 {
 	struct snd_soc_component *component = dai->component;
+	struct snd_soc_dapm_context *dapm = snd_soc_component_to_dapm(component);
 	struct wm8962_priv *wm8962 = snd_soc_component_get_drvdata(component);
 	int i;
 	int aif0 = 0;
@@ -2678,7 +2682,7 @@ static int wm8962_hw_params(struct snd_pcm_substream *substream,
 	dev_dbg(component->dev, "hw_params set BCLK %dHz LRCLK %dHz\n",
 		wm8962->bclk, wm8962->lrclk);
 
-	if (snd_soc_component_get_bias_level(component) == SND_SOC_BIAS_ON)
+	if (snd_soc_dapm_get_bias_level(dapm) == SND_SOC_BIAS_ON)
 		wm8962_configure_bclk(component);
 
 	return 0;
@@ -3189,7 +3193,7 @@ static irqreturn_t wm8962_irq(int irq, void *data)
 int wm8962_mic_detect(struct snd_soc_component *component, struct snd_soc_jack *jack)
 {
 	struct wm8962_priv *wm8962 = snd_soc_component_get_drvdata(component);
-	struct snd_soc_dapm_context *dapm = snd_soc_component_get_dapm(component);
+	struct snd_soc_dapm_context *dapm = snd_soc_component_to_dapm(component);
 	int irq_mask, enable;
 
 	wm8962->jack = jack;
@@ -3235,7 +3239,7 @@ static void wm8962_beep_work(struct work_struct *work)
 	struct wm8962_priv *wm8962 =
 		container_of(work, struct wm8962_priv, beep_work);
 	struct snd_soc_component *component = wm8962->component;
-	struct snd_soc_dapm_context *dapm = snd_soc_component_get_dapm(component);
+	struct snd_soc_dapm_context *dapm = snd_soc_component_to_dapm(component);
 	int i;
 	int reg = 0;
 	int best = 0;
@@ -3481,7 +3485,7 @@ static void wm8962_free_gpio(struct snd_soc_component *component)
 
 static int wm8962_probe(struct snd_soc_component *component)
 {
-	struct snd_soc_dapm_context *dapm = snd_soc_component_get_dapm(component);
+	struct snd_soc_dapm_context *dapm = snd_soc_component_to_dapm(component);
 	int ret;
 	struct wm8962_priv *wm8962 = snd_soc_component_get_drvdata(component);
 	int i;
@@ -3536,7 +3540,7 @@ static int wm8962_probe(struct snd_soc_component *component)
 	}
 	if (!dmicclk || !dmicdat) {
 		dev_dbg(component->dev, "DMIC not in use, disabling\n");
-		snd_soc_dapm_nc_pin(dapm, "DMICDAT");
+		snd_soc_dapm_disable_pin(dapm, "DMICDAT");
 	}
 	if (dmicclk != dmicdat)
 		dev_warn(component->dev, "DMIC GPIOs partially configured\n");
