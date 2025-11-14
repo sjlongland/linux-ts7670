731ded6dd7c02e2d8d16caf7d99992001d0697b9
John Johansen <john.johansen@canonical.com>
2025-11-14 00:14:36 -0800

    apparmor: remove apply_modes_to_perms from label_match
    The modes shouldn't be applied at the point of label match, it just
    results in them being applied multiple times. Instead they should be
    applied after which is already being done by all callers so it can
    just be dropped from label_match.
    
    Reviewed-by: Georgia Garcia <georgia.garcia@canonical.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/label.c
+++ security/apparmor/label.c
@@ -1312,7 +1312,6 @@ next:
 			goto fail;
 	}
 	*perms = *aa_lookup_perms(rules->policy, state);
-	aa_apply_modes_to_perms(profile, perms);
 	if ((perms->allow & request) != request)
 		return -EACCES;
 
@@ -1365,7 +1364,6 @@ static int label_components_match(struct aa_profile *profile,
 
 next:
 	tmp = *aa_lookup_perms(rules->policy, state);
-	aa_apply_modes_to_perms(profile, &tmp);
 	aa_perms_accum(perms, &tmp);
 	label_for_each_cont(i, label, tp) {
 		if (!aa_ns_visible(profile->ns, tp->ns, subns))
@@ -1374,7 +1372,6 @@ next:
 		if (!state)
 			goto fail;
 		tmp = *aa_lookup_perms(rules->policy, state);
-		aa_apply_modes_to_perms(profile, &tmp);
 		aa_perms_accum(perms, &tmp);
 	}
 
