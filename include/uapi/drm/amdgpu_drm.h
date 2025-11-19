d42d6e1f4533c7292509b7a9687140f95781a6a9
Timur Kristóf <timur.kristof@gmail.com>
2025-11-19 10:25:43 +0100

    drm/amdgpu/uapi: Clarify comment on AMDGPU_VM_PAGE_PRT
    In the context of the amdgpu uAPI, the PRT flag is referring only
    to unmapped pages of a partially resident texture (aka. sparse
    resource), but not the full resource.
    
    Virtual addresses marked with this flag behave as follows:
    - Reads return zero
    - Writes are discarded
    
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- include/uapi/drm/amdgpu_drm.h
+++ include/uapi/drm/amdgpu_drm.h
@@ -570,7 +570,7 @@ struct drm_amdgpu_gem_op {
 #define AMDGPU_VM_PAGE_WRITEABLE	(1 << 2)
 /* executable mapping, new for VI */
 #define AMDGPU_VM_PAGE_EXECUTABLE	(1 << 3)
-/* partially resident texture */
+/* unmapped page of partially resident textures */
 #define AMDGPU_VM_PAGE_PRT		(1 << 4)
 /* MTYPE flags use bit 5 to 8 */
 #define AMDGPU_VM_MTYPE_MASK		(0xf << 5)
