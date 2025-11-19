328e2ced4f725d53309683d54a322240aa3f4403
Coiby Xu <coxu@redhat.com>
2025-11-19 22:03:25 +0800

    ima: Access decompressed kernel module to verify appended signature
    Currently, when in-kernel module decompression (CONFIG_MODULE_DECOMPRESS)
    is enabled, IMA has no way to verify the appended module signature as it
    can't decompress the module.
    
    Define a new kernel_read_file_id enumerate READING_MODULE_COMPRESSED so
    IMA can calculate the compressed kernel module data hash on
    READING_MODULE_COMPRESSED and defer appraising/measuring it until on
    READING_MODULE when the module has been decompressed.
    
    Before enabling in-kernel module decompression, a kernel module in
    initramfs can still be loaded with ima_policy=secure_boot. So adjust the
    kernel module rule in secure_boot policy to allow either an IMA
    signature OR an appended signature i.e. to use
    "appraise func=MODULE_CHECK appraise_type=imasig|modsig".
    
    Reported-by: Karel Srot <ksrot@redhat.com>
    Suggested-by: Mimi Zohar <zohar@linux.ibm.com>
    Suggested-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Coiby Xu <coxu@redhat.com>
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -4134,7 +4134,7 @@ static int selinux_kernel_read_file(struct file *file,
 {
 	int rc = 0;
 
-	BUILD_BUG_ON_MSG(READING_MAX_ID > 7,
+	BUILD_BUG_ON_MSG(READING_MAX_ID > 8,
 			 "New kernel_read_file_id introduced; update SELinux!");
 
 	switch (id) {
@@ -4142,6 +4142,7 @@ static int selinux_kernel_read_file(struct file *file,
 		rc = selinux_kernel_load_from_file(file, SYSTEM__FIRMWARE_LOAD);
 		break;
 	case READING_MODULE:
+	case READING_MODULE_COMPRESSED:
 		rc = selinux_kernel_load_from_file(file, SYSTEM__MODULE_LOAD);
 		break;
 	case READING_KEXEC_IMAGE:
@@ -4170,7 +4171,7 @@ static int selinux_kernel_load_data(enum kernel_load_data_id id, bool contents)
 {
 	int rc = 0;
 
-	BUILD_BUG_ON_MSG(LOADING_MAX_ID > 7,
+	BUILD_BUG_ON_MSG(LOADING_MAX_ID > 8,
 			 "New kernel_load_data_id introduced; update SELinux!");
 
 	switch (id) {
