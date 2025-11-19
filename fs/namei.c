616d586b7cd94acbc08a90cb91aad906aea681d7
Mateusz Guzik <mjguzik@gmail.com>
2025-11-19 15:49:30 +0100

    fs: mark lookup_slow() as noinline
    Otherwise it gets inlined notably in walk_component(), which convinces
    the compiler to push/pop additional registers in the fast path to
    accomodate existence of the inlined version.
    
    Shortens the fast path of that routine from 87 to 71 bytes.
    
    Signed-off-by: Mateusz Guzik <mjguzik@gmail.com>
    Link: https://patch.msgid.link/20251119144930.2911698-1-mjguzik@gmail.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/namei.c
+++ fs/namei.c
@@ -1854,7 +1854,7 @@ again:
 	return dentry;
 }
 
-static struct dentry *lookup_slow(const struct qstr *name,
+static noinline struct dentry *lookup_slow(const struct qstr *name,
 				  struct dentry *dir,
 				  unsigned int flags)
 {
