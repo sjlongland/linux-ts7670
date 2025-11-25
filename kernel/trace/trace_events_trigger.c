b38f1a98ef273c3805f9f4c3186be3146304232b
Steven Rostedt <rostedt@goodmis.org>
2025-11-25 16:40:07 -0500

    tracing: Use strim() in trigger_process_regex() instead of skip_spaces()
    The function trigger_process_regex() is called by a few functions, where
    only one calls strim() on the buffer passed to it. That leaves the other
    functions not trimming the end of the buffer passed in and making it a
    little inconsistent.
    
    Remove the strim() from event_trigger_regex_write() and have
    trigger_process_regex() use strim() instead of skip_spaces(). The buff
    variable is not passed in as const, so it can be modified.
    
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Tom Zanussi <zanussi@kernel.org>
    Link: https://patch.msgid.link/20251125214032.323747707@kernel.org
    Acked-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/trace/trace_events_trigger.c
+++ kernel/trace/trace_events_trigger.c
@@ -245,7 +245,8 @@ int trigger_process_regex(struct trace_event_file *file, char *buff)
 	char *command, *next;
 	struct event_command *p;
 
-	next = buff = skip_spaces(buff);
+	next = buff = strim(buff);
+
 	command = strsep(&next, ": \t");
 	if (next) {
 		next = skip_spaces(next);
@@ -281,8 +282,6 @@ static ssize_t event_trigger_regex_write(struct file *file,
 	buf = memdup_user_nul(ubuf, cnt);
 	if (IS_ERR(buf))
 		return PTR_ERR(buf);
-
-	strim(buf);
 
 	guard(mutex)(&event_mutex);
 
