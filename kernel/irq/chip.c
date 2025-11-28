4682c5df5df7d356e15d6ddae9c28a9a09e567c1
Radu Rendec <rrendec@redhat.com>
2025-11-28 16:20:53 -0500

    genirq: Add interrupt redirection infrastructure
    Add infrastructure to redirect interrupt handler execution to a
    different CPU when the current CPU is not part of the interrupt's CPU
    affinity mask.
    
    This is primarily aimed at (de)multiplexed interrupts, where the child
    interrupt handler runs in the context of the parent interrupt handler,
    and therefore CPU affinity control for the child interrupt is typically
    not available.
    
    With the new infrastructure, the child interrupt is allowed to freely
    change its affinity setting, independently of the parent. If the
    interrupt handler happens to be triggered on an "incompatible" CPU (a
    CPU that's not part of the child interrupt's affinity mask), the handler
    is redirected and runs in IRQ work context on a "compatible" CPU.
    
    No functional change is being made to any existing irqchip driver, and
    irqchip drivers must be explicitly modified to use the newly added
    infrastructure to support interrupt redirection.
    
    Originally-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Radu Rendec <rrendec@redhat.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/linux-pci/878qpg4o4t.ffs@tglx/
    Link: https://patch.msgid.link/20251128212055.1409093-2-rrendec@redhat.com

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -1122,7 +1122,7 @@ void irq_cpu_offline(void)
 }
 #endif
 
-#ifdef	CONFIG_IRQ_DOMAIN_HIERARCHY
+#ifdef CONFIG_IRQ_DOMAIN_HIERARCHY
 
 #ifdef CONFIG_IRQ_FASTEOI_HIERARCHY_HANDLERS
 /**
@@ -1226,6 +1226,15 @@ out:
 EXPORT_SYMBOL_GPL(handle_fasteoi_mask_irq);
 
 #endif /* CONFIG_IRQ_FASTEOI_HIERARCHY_HANDLERS */
+
+#ifdef CONFIG_SMP
+void irq_chip_pre_redirect_parent(struct irq_data *data)
+{
+	data = data->parent_data;
+	data->chip->irq_pre_redirect(data);
+}
+EXPORT_SYMBOL_GPL(irq_chip_pre_redirect_parent);
+#endif
 
 /**
  * irq_chip_set_parent_state - set the state of a parent interrupt.
@@ -1509,6 +1518,17 @@ void irq_chip_release_resources_parent(struct irq_data *data)
 		data->chip->irq_release_resources(data);
 }
 EXPORT_SYMBOL_GPL(irq_chip_release_resources_parent);
+#endif /* CONFIG_IRQ_DOMAIN_HIERARCHY */
+
+#ifdef CONFIG_SMP
+int irq_chip_redirect_set_affinity(struct irq_data *data, const struct cpumask *dest, bool force)
+{
+	struct irq_redirect *redir = &irq_data_to_desc(data)->redirect;
+
+	WRITE_ONCE(redir->target_cpu, cpumask_first(dest));
+	return IRQ_SET_MASK_OK;
+}
+EXPORT_SYMBOL_GPL(irq_chip_redirect_set_affinity);
 #endif
 
 /**
