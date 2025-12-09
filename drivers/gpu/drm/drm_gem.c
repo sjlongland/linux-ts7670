9993de13179a419991592841456eaf4b6089fdd8
Boris Brezillon <boris.brezillon@collabora.com>
2025-12-09 18:11:51 +0100

    drm/gem: Fix builds with CONFIG_MMU=n
    drm_gem_get_unmapped_area() relies on mm_get_unmapped_area() which is
    only available if CONFIG_MMU=y.
    
    Fixes: 99bda20d6d4c ("drm/gem: Introduce drm_gem_get_unmapped_area() fop")
    Cc: Loïc Molinari <loic.molinari@collabora.com>
    Reviewed-by: Loïc Molinari <loic.molinari@collabora.com>
    Link: https://patch.msgid.link/20251209171151.2449120-1-boris.brezillon@collabora.com
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

--- drivers/gpu/drm/drm_gem.c
+++ drivers/gpu/drm/drm_gem.c
@@ -1172,6 +1172,7 @@ drm_gem_object_lookup_at_offset(struct file *filp, unsigned long start,
 	return obj;
 }
 
+#ifdef CONFIG_MMU
 /**
  * drm_gem_get_unmapped_area - get memory mapping region routine for GEM objects
  * @filp: DRM file pointer
@@ -1214,6 +1215,7 @@ unsigned long drm_gem_get_unmapped_area(struct file *filp, unsigned long uaddr,
 	return ret;
 }
 EXPORT_SYMBOL_GPL(drm_gem_get_unmapped_area);
+#endif
 
 /**
  * drm_gem_mmap - memory map routine for GEM objects
