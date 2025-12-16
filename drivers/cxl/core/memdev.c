5699b50d0339c05469a8fbd245da6c05aaad7a91
Dan Williams <dan.j.williams@intel.com>
2025-12-15 16:56:15 -0800

    cxl/mem: Drop @host argument to devm_cxl_add_memdev()
    In all cases the device that created the 'struct cxl_dev_state' instance is
    also the device to host the devm cleanup of devm_cxl_add_memdev(). This
    simplifies the function prototype, and limits a degree of freedom of the
    API.
    
    Cc: Smita Koralahalli <Smita.KoralahalliChannabasappa@amd.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Reviewed-by: Alison Schofield <alison.schofield@intel.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Ben Cheatham <benjamin.cheatham@amd.com>
    Tested-by: Alejandro Lucero <alucerop@amd.com>
    Link: https://patch.msgid.link/20251216005616.3090129-6-dan.j.williams@intel.com
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/cxl/core/memdev.c
+++ drivers/cxl/core/memdev.c
@@ -1079,8 +1079,7 @@ static struct cxl_memdev *cxl_memdev_autoremove(struct cxl_memdev *cxlmd)
  * Core helper for devm_cxl_add_memdev() that wants to both create a device and
  * assert to the caller that upon return cxl_mem::probe() has been invoked.
  */
-struct cxl_memdev *__devm_cxl_add_memdev(struct device *host,
-					 struct cxl_dev_state *cxlds)
+struct cxl_memdev *__devm_cxl_add_memdev(struct cxl_dev_state *cxlds)
 {
 	struct device *dev;
 	int rc;
