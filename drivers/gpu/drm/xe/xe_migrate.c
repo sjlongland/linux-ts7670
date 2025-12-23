720e21ac727b7b2d53832aad7c0885f5f49a9af9
Francois Dugast <francois.dugast@intel.com>
2025-12-23 12:53:27 +0100

    drm/xe/migrate: Configure migration queue as low latency
    Commit 5488bec96bcc ("drm/xe/uapi: Use hint for guc to set GT frequency")
    introduced low latency hint for use by user space when creating an exec
    queue. This instructs SLPC to ramp the GT frequency aggressively.
    
    SVM relies on an internal exec queue to migrate memory upon page faults.
    This change creates this exec queue with the low latency hint to speed up
    migration.
    
    This should not impact systems where GT frequency is set over sysfs, or
    with long running workloads which give enough time for the frequency to
    ramp up. An example of memory access pattern that shows an improvement of
    SVM performance is running hundreds of times IGT eu-fault-2m-once-device
    in xe_exec_system_allocator. The copy duration provided by GT stats in
    svm_2M_device_copy_us shows per GPU page fault:
        ~ 165 μs without low latency hint
        ~ 130 μs with low latency hint
    
    Suggested-by: Matthew Brost <matthew.brost@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Francois Dugast <francois.dugast@intel.com>
    Link: https://patch.msgid.link/20251223115327.49555-1-francois.dugast@intel.com
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/xe_migrate.c
+++ drivers/gpu/drm/xe/xe_migrate.c
@@ -439,7 +439,8 @@ struct xe_migrate *xe_migrate_init(struct xe_tile *tile)
 					    EXEC_QUEUE_FLAG_KERNEL |
 					    EXEC_QUEUE_FLAG_PERMANENT |
 					    EXEC_QUEUE_FLAG_HIGH_PRIORITY |
-					    EXEC_QUEUE_FLAG_MIGRATE, 0);
+					    EXEC_QUEUE_FLAG_MIGRATE |
+					    EXEC_QUEUE_FLAG_LOW_LATENCY, 0);
 	} else {
 		m->q = xe_exec_queue_create_class(xe, primary_gt, vm,
 						  XE_ENGINE_CLASS_COPY,
