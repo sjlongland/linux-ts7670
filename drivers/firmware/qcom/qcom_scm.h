7e3983ed2d62d9a718b6fb7400063139262eb1d0
Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
2026-01-05 18:52:59 +0530

    firmware: qcom_scm: Add qcom_scm_pas_get_rsc_table() to get resource table
    Qualcomm remote processor may rely on Static and Dynamic resources for
    it to be functional. Static resources are fixed like for example,
    memory-mapped addresses required by the subsystem and dynamic
    resources, such as shared memory in DDR etc., are determined at
    runtime during the boot process.
    
    For most of the Qualcomm SoCs, when run with Gunyah or older QHEE
    hypervisor, all the resources whether it is static or dynamic, is
    managed by the hypervisor. Dynamic resources if it is present for a
    remote processor will always be coming from secure world via SMC call
    while static resources may be present in remote processor firmware
    binary or it may be coming qcom_scm_pas_get_rsc_table() SMC call along
    with dynamic resources.
    
    Some of the remote processor drivers, such as video, GPU, IPA, etc., do
    not check whether resources are present in their remote processor
    firmware binary. In such cases, the caller of this function should set
    input_rt and input_rt_size as NULL and zero respectively. Remoteproc
    framework has method to check whether firmware binary contain resources
    or not and they should be pass resource table pointer to input_rt and
    resource table size to input_rt_size and this will be forwarded to
    TrustZone for authentication. TrustZone will then append the dynamic
    resources and return the complete resource table in the passed output
    buffer.
    
    More about documentation on resource table format can be found in
    include/linux/remoteproc.h
    
    Signed-off-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260105-kvmrprocv10-v10-11-022e96815380@oss.qualcomm.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/firmware/qcom/qcom_scm.h
+++ drivers/firmware/qcom/qcom_scm.h
@@ -101,6 +101,7 @@ struct qcom_tzmem_pool *qcom_scm_get_tzmem_pool(void);
 #define QCOM_SCM_PIL_PAS_SHUTDOWN	0x06
 #define QCOM_SCM_PIL_PAS_IS_SUPPORTED	0x07
 #define QCOM_SCM_PIL_PAS_MSS_RESET	0x0a
+#define QCOM_SCM_PIL_PAS_GET_RSCTABLE	0x21
 
 #define QCOM_SCM_SVC_IO			0x05
 #define QCOM_SCM_IO_READ		0x01
