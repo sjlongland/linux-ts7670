df2570738b20459eb61df0eaaf9a53e28279f793
Christoph Hellwig <hch@lst.de>
2026-01-07 08:27:07 +0100

    NFS: simplify the detached delegation check in update_open_stateid
    When nfs_detach_delegation_locked detaches a delegation from an inode,
    it clears both nfsi->delegation and delegation->inode.  Use the later
    in update_open_stateid to check for a detached inode, as that avoids
    an extra local variable, and removes the need for a RCU derefernence
    as we already hold the lock in the delegation.  This prepares for
    removing the surrounding RCU critical section.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Anna Schumaker <anna.schumaker@oracle.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -1892,7 +1892,6 @@ int update_open_stateid(struct nfs4_state *state,
 {
 	struct nfs_server *server = NFS_SERVER(state->inode);
 	struct nfs_client *clp = server->nfs_client;
-	struct nfs_inode *nfsi = NFS_I(state->inode);
 	struct nfs_delegation *deleg_cur;
 	nfs4_stateid freeme = { };
 	int ret = 0;
@@ -1911,7 +1910,7 @@ int update_open_stateid(struct nfs4_state *state,
 		goto no_delegation;
 
 	spin_lock(&deleg_cur->lock);
-	if (rcu_dereference(nfsi->delegation) != deleg_cur ||
+	if (!deleg_cur->inode ||
 	   test_bit(NFS_DELEGATION_RETURNING, &deleg_cur->flags) ||
 	    (deleg_cur->type & fmode) != fmode)
 		goto no_delegation_unlock;
