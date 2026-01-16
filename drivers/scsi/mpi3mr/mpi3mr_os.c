eea59769d5257e2d63a6de30fa39a932c9fe0ec1
Ranjan Kumar <ranjan.kumar@broadcom.com>
2026-01-16 11:37:13 +0530

    scsi: mpi3mr: Rename log data save helper to reflect threaded/BH context
    Log data events can be processed from BH and threaded contexts.  Rename
    the save helper to document its intended usage and improve readability
    of the event handling flow.
    
    Signed-off-by: Ranjan Kumar <ranjan.kumar@broadcom.com>
    Link: https://patch.msgid.link/20260116060719.32937-3-ranjan.kumar@broadcom.com
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/mpi3mr/mpi3mr_os.c
+++ drivers/scsi/mpi3mr/mpi3mr_os.c
@@ -1939,7 +1939,7 @@ static void mpi3mr_pcietopochg_evt_bh(struct mpi3mr_ioc *mrioc,
 static void mpi3mr_logdata_evt_bh(struct mpi3mr_ioc *mrioc,
 	struct mpi3mr_fwevt *fwevt)
 {
-	mpi3mr_app_save_logdata(mrioc, fwevt->event_data,
+	mpi3mr_app_save_logdata_th(mrioc, fwevt->event_data,
 	    fwevt->event_data_size);
 }
 
@@ -3020,6 +3020,11 @@ void mpi3mr_os_handle_events(struct mpi3mr_ioc *mrioc,
 	}
 	case MPI3_EVENT_DEVICE_INFO_CHANGED:
 	case MPI3_EVENT_LOG_DATA:
+
+		sz = event_reply->event_data_length * 4;
+		mpi3mr_app_save_logdata_th(mrioc,
+			(char *)event_reply->event_data, sz);
+		break;
 	case MPI3_EVENT_ENCL_DEVICE_STATUS_CHANGE:
 	case MPI3_EVENT_ENCL_DEVICE_ADDED:
 	{
