d1a25b65621609f5210f611cf90f7479d1cfeae1
Ming Lei <ming.lei@redhat.com>
2026-01-16 15:46:37 +0800

    block: pass io_comp_batch to rq_end_io_fn callback
    Add a third parameter 'const struct io_comp_batch *' to the rq_end_io_fn
    callback signature. This allows end_io handlers to access the completion
    batch context when requests are completed via blk_mq_end_request_batch().
    
    The io_comp_batch is passed from blk_mq_end_request_batch(), while NULL
    is passed from __blk_mq_end_request() and blk_mq_put_rq_ref() which don't
    have batch context.
    
    This infrastructure change enables drivers to detect whether they're
    being called from a batched completion path (like iopoll) and access
    additional context stored in the io_comp_batch.
    
    Update all rq_end_io_fn implementations:
    - block/blk-mq.c: blk_end_sync_rq
    - block/blk-flush.c: flush_end_io, mq_flush_data_end_io
    - drivers/nvme/host/ioctl.c: nvme_uring_cmd_end_io
    - drivers/nvme/host/core.c: nvme_keep_alive_end_io
    - drivers/nvme/host/pci.c: abort_endio, nvme_del_queue_end, nvme_del_cq_end
    - drivers/nvme/target/passthru.c: nvmet_passthru_req_done
    - drivers/scsi/scsi_error.c: eh_lock_door_done
    - drivers/scsi/sg.c: sg_rq_end_io
    - drivers/scsi/st.c: st_scsi_execute_end
    - drivers/target/target_core_pscsi.c: pscsi_req_done
    - drivers/md/dm-rq.c: end_clone_request
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Reviewed-by: Kanchan Joshi <joshi.k@samsung.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/nvme/host/ioctl.c
+++ drivers/nvme/host/ioctl.c
@@ -414,7 +414,8 @@ static void nvme_uring_task_cb(struct io_uring_cmd *ioucmd,
 }
 
 static enum rq_end_io_ret nvme_uring_cmd_end_io(struct request *req,
-						blk_status_t err)
+						blk_status_t err,
+						const struct io_comp_batch *iob)
 {
 	struct io_uring_cmd *ioucmd = req->end_io_data;
 	struct nvme_uring_cmd_pdu *pdu = nvme_uring_cmd_pdu(ioucmd);
--- include/linux/blk-mq.h
+++ include/linux/blk-mq.h
@@ -12,6 +12,7 @@
 
 struct blk_mq_tags;
 struct blk_flush_queue;
+struct io_comp_batch;
 
 #define BLKDEV_MIN_RQ	4
 #define BLKDEV_DEFAULT_RQ	128
@@ -21,7 +22,8 @@ enum rq_end_io_ret {
 	RQ_END_IO_FREE,
 };
 
-typedef enum rq_end_io_ret (rq_end_io_fn)(struct request *, blk_status_t);
+typedef enum rq_end_io_ret (rq_end_io_fn)(struct request *, blk_status_t,
+					  const struct io_comp_batch *);
 
 /*
  * request flags */
