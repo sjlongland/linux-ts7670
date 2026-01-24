7a17985e0744b2189816e5210a9e0ead32587283
Menglong Dong <menglong8.dong@gmail.com>
2026-01-24 14:19:57 +0800

    bpf: use the least significant byte for the nr_args in trampoline
    For now, ((u64 *)ctx)[-1] is used to store the nr_args in the trampoline.
    However, 1 byte is enough to store such information. Therefore, we use
    only the least significant byte of ((u64 *)ctx)[-1] to store the nr_args,
    and reserve the rest for other usages.
    
    Signed-off-by: Menglong Dong <dongml2@chinatelecom.cn>
    Link: https://lore.kernel.org/r/20260124062008.8657-3-dongml2@chinatelecom.cn
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -1196,7 +1196,7 @@ static const struct bpf_func_proto bpf_get_branch_snapshot_proto = {
 BPF_CALL_3(get_func_arg, void *, ctx, u32, n, u64 *, value)
 {
 	/* This helper call is inlined by verifier. */
-	u64 nr_args = ((u64 *)ctx)[-1];
+	u64 nr_args = ((u64 *)ctx)[-1] & 0xFF;
 
 	if ((u64) n >= nr_args)
 		return -EINVAL;
@@ -1216,7 +1216,7 @@ static const struct bpf_func_proto bpf_get_func_arg_proto = {
 BPF_CALL_2(get_func_ret, void *, ctx, u64 *, value)
 {
 	/* This helper call is inlined by verifier. */
-	u64 nr_args = ((u64 *)ctx)[-1];
+	u64 nr_args = ((u64 *)ctx)[-1] & 0xFF;
 
 	*value = ((u64 *)ctx)[nr_args];
 	return 0;
@@ -1233,7 +1233,7 @@ static const struct bpf_func_proto bpf_get_func_ret_proto = {
 BPF_CALL_1(get_func_arg_cnt, void *, ctx)
 {
 	/* This helper call is inlined by verifier. */
-	return ((u64 *)ctx)[-1];
+	return ((u64 *)ctx)[-1] & 0xFF;
 }
 
 static const struct bpf_func_proto bpf_get_func_arg_cnt_proto = {
