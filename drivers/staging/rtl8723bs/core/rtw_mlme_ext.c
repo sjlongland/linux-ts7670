b9b040e1eadef8db5161038825f5f1e5cad61d06
Michael Huang <tehsiu.huang@gmail.com>
2026-01-24 15:15:57 -0800

    staging: rtl8723bs: use !ptr instead of ptr == NULL
    Simplify pointer null checks by using the "!ptr" convention instead of
    the more verbose "ptr == NULL" comparison.
    
    Signed-off-by: Michael Huang <tehsiu.huang@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@linaro.org>
    Link: https://patch.msgid.link/20260124231557.18189-8-tehsiu.huang@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_mlme_ext.c
+++ drivers/staging/rtl8723bs/core/rtw_mlme_ext.c
@@ -6044,8 +6044,7 @@ u8 run_in_thread_hdl(struct adapter *padapter, u8 *pbuf)
 {
 	struct RunInThread_param *p;
 
-
-	if (pbuf == NULL)
+	if (!pbuf)
 		return H2C_PARAMETERS_ERROR;
 	p = (struct RunInThread_param *)pbuf;
 
