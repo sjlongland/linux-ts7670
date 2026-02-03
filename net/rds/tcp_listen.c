d8cccc78610cb30db7c509d466200a5fb12d82c7
Gerd Rausch <gerd.rausch@oracle.com>
2026-02-02 22:57:19 -0700

    net/rds: Kick-start TCP receiver after accept
    In cases where the server (the node with the higher IP-address)
    in an RDS/TCP connection is overwhelmed it is possible that the
    socket that was just accepted is chock-full of messages, up to
    the limit of what the socket receive buffer permits.
    
    Subsequently, "rds_tcp_data_ready" won't be called anymore,
    because there is no more space to receive additional messages.
    
    Nor was it called prior to the point of calling "rds_tcp_set_callbacks",
    because the "sk_data_ready" pointer didn't even point to
    "rds_tcp_data_ready" yet.
    
    We fix this by simply kick-starting the receive-worker
    for all cases where the socket state is neither
    "TCP_CLOSE_WAIT" nor "TCP_CLOSE".
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Link: https://patch.msgid.link/20260203055723.1085751-5-achender@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/rds/tcp_listen.c
+++ net/rds/tcp_listen.c
@@ -220,6 +220,8 @@ int rds_tcp_accept_one(struct socket *sock)
 	    new_sock->sk->sk_state == TCP_LAST_ACK ||
 	    new_sock->sk->sk_state == TCP_CLOSE)
 		rds_conn_path_drop(cp, 0);
+	else
+		queue_delayed_work(cp->cp_wq, &cp->cp_recv_w, 0);
 
 	new_sock = NULL;
 	ret = 0;
