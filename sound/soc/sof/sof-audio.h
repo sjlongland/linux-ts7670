d7e099a9e1f2fb0b37f3c6c8c87d238ecaa341cd
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2026-02-04 10:18:28 +0200

    ASoC: SOF: ipc4-topology: Add new tokens for pipeline direction
    Parse the pipeline direction from topology. The direction_valid token is
    required for backward-compatibility with older topologies that may not
    have the direction set for pipelines. This will be used when
    setting up pipelines to check if a pipeline is in the same direction as
    the requested params and skip those in the opposite direction like in
    the case of echo reference capture pipelines during playback.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Link: https://patch.msgid.link/20260204081833.16630-6-peter.ujfalusi@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/sof-audio.h
+++ sound/soc/sof/sof-audio.h
@@ -510,6 +510,9 @@ struct snd_sof_widget {
  * @complete: flag used to indicate that pipeline set up is complete.
  * @core_mask: Mask containing target cores for all modules in the pipeline
  * @list: List item in sdev pipeline_list
+ * @direction_valid: flag indicating if the direction is set in topology
+ * @direction: pipeline direction set in topology, valid is direction_valid is true
+ *
  */
 struct snd_sof_pipeline {
 	struct snd_sof_widget *pipe_widget;
@@ -518,6 +521,8 @@ struct snd_sof_pipeline {
 	int complete;
 	unsigned long core_mask;
 	struct list_head list;
+	bool direction_valid;
+	u32 direction;
 };
 
 /* ASoC SOF DAPM route */
