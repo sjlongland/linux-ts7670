4862a2ea163d8712196a75ce99d2bc4c06127b76
Andy Yan <andy.yan@rock-chips.com>
2026-02-06 09:04:13 +0800

    drm/bridge: synopsys: dw-dp: Make i2s/spdif clk optional
    The i2s/spdif clk are mandatory for rk3588, but not used
    for the upcoming rk3576, so make it optional here.
    
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>
    Reviewed-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Tested-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Link: https://patch.msgid.link/20260206010421.443605-4-andyshrk@163.com

--- drivers/gpu/drm/bridge/synopsys/dw-dp.c
+++ drivers/gpu/drm/bridge/synopsys/dw-dp.c
@@ -2020,13 +2020,13 @@ struct dw_dp *dw_dp_bind(struct device *dev, struct drm_encoder *encoder,
 		return ERR_CAST(dp->aux_clk);
 	}
 
-	dp->i2s_clk = devm_clk_get(dev, "i2s");
+	dp->i2s_clk = devm_clk_get_optional(dev, "i2s");
 	if (IS_ERR(dp->i2s_clk)) {
 		dev_err_probe(dev, PTR_ERR(dp->i2s_clk), "failed to get i2s clock\n");
 		return ERR_CAST(dp->i2s_clk);
 	}
 
-	dp->spdif_clk = devm_clk_get(dev, "spdif");
+	dp->spdif_clk = devm_clk_get_optional(dev, "spdif");
 	if (IS_ERR(dp->spdif_clk)) {
 		dev_err_probe(dev, PTR_ERR(dp->spdif_clk), "failed to get spdif clock\n");
 		return ERR_CAST(dp->spdif_clk);
