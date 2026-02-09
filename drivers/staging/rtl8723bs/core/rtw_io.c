7d8c89709b7bde3d298b90f75896dcfbb69d10ed
Siwanan Bungtong <horstaufmental@gmail.com>
2026-02-09 10:10:34 +0700

    staging: rtl8723bs: Wrap long function parameter lists
    Wrap long function parameter lists to comply with kernel
    coding style and avoid checkpatch warnings.
    
    Signed-off-by: Siwanan Bungtong <horstaufmental@gmail.com>
    Link: https://patch.msgid.link/20260209031034.130269-1-horstaufmental@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_io.c
+++ drivers/staging/rtl8723bs/core/rtw_io.c
@@ -116,7 +116,9 @@ u32 rtw_write_port(struct adapter *adapter, u32 addr, u32 cnt, u8 *pmem)
 	return _write_port(pintfhdl, addr, cnt, pmem);
 }
 
-int rtw_init_io_priv(struct adapter *padapter, void (*set_intf_ops)(struct adapter *padapter, struct _io_ops *pops))
+int rtw_init_io_priv(struct adapter *padapter,
+		     void (*set_intf_ops)(struct adapter *padapter,
+					  struct _io_ops *pops))
 {
 	struct io_priv *piopriv = &padapter->iopriv;
 	struct intf_hdl *pintf = &piopriv->intf;
