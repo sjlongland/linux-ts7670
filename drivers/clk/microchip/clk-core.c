0e6535adb424768f47de231caa8cb995b0736770
Thomas Bogendoerfer <tsbogend@alpha.franken.de>
2026-02-10 16:48:59 +0100

    Revert "clk: microchip: core: allow driver to be compiled with COMPILE_TEST"
    This reverts commit 026d70dcfe5de1543bb8edb8e50d22dc16863e6b.
    
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- drivers/clk/microchip/clk-core.c
+++ drivers/clk/microchip/clk-core.c
@@ -74,7 +74,6 @@
 /* SoC specific clock needed during SPLL clock rate switch */
 static struct clk_hw *pic32_sclk_hw;
 
-#ifdef CONFIG_MATCH_PIC32
 /* add instruction pipeline delay while CPU clock is in-transition. */
 #define cpu_nop5()			\
 do {					\
@@ -84,9 +83,6 @@ do {					\
 	__asm__ __volatile__("nop");	\
 	__asm__ __volatile__("nop");	\
 } while (0)
-#else
-#define cpu_nop5()
-#endif
 
 /* Perpheral bus clocks */
 struct pic32_periph_clk {
