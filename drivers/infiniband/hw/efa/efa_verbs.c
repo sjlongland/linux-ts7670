0cfee71aaf0b0aeded684ac9603c6712c8a08bf4
Leon Romanovsky <leonro@nvidia.com>
2026-02-13 12:57:45 +0200

    RDMA/efa: Remove check for zero CQE count
    Since ib_core now handles validation, the device driver no longer needs
    to verify that the CQE count is non‑zero.
    
    Link: https://patch.msgid.link/20260213-refactor-umem-v1-9-f3be85847922@nvidia.com
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- drivers/infiniband/hw/efa/efa_verbs.c
+++ drivers/infiniband/hw/efa/efa_verbs.c
@@ -1144,9 +1144,9 @@ int efa_create_cq(struct ib_cq *ibcq, const struct ib_cq_init_attr *attr,
 	if (attr->flags)
 		return -EOPNOTSUPP;
 
-	if (entries < 1 || entries > dev->dev_attr.max_cq_depth) {
+	if (entries > dev->dev_attr.max_cq_depth) {
 		ibdev_dbg(ibdev,
-			  "cq: requested entries[%u] non-positive or greater than max[%u]\n",
+			  "cq: requested entries[%u] greater than max[%u]\n",
 			  entries, dev->dev_attr.max_cq_depth);
 		err = -EINVAL;
 		goto err_out;
