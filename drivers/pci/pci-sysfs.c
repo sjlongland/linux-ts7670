c641b6ea39962afe1da354872cf7b108013333da
Keith Busch <kbusch@kernel.org>
2026-02-17 08:08:36 -0800

    PCI: Make reset_subordinate hotplug safe
    Use the slot reset method when resetting the bridge if the bus contains
    hot plug slots. This fixes spurious hot plug events that are triggered
    by the secondary bus reset that bypasses the slot's detection disabling.
    
    Resetting a bridge's subordinate bus can be done like this:
    
      # echo 1 > /sys/bus/pci/devices/0000:50:01.0/reset_subordinate
    
    Prior to this patch, an example kernel message may show something like:
    
      pcieport 0000:50:01.0: pciehp: Slot(40): Link Down
    
    With this change, the pciehp driver ignores the link event during the
    reset, so may show this message instead:
    
      pcieport 0000:50:01.0: pciehp: Slot(40): Link Down/Up ignored
    
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Link: https://patch.msgid.link/20260217160836.2709885-4-kbusch@meta.com

--- drivers/pci/pci-sysfs.c
+++ drivers/pci/pci-sysfs.c
@@ -546,7 +546,6 @@ static ssize_t reset_subordinate_store(struct device *dev,
 				const char *buf, size_t count)
 {
 	struct pci_dev *pdev = to_pci_dev(dev);
-	struct pci_bus *bus = pdev->subordinate;
 	unsigned long val;
 
 	if (!capable(CAP_SYS_ADMIN))
@@ -556,7 +555,7 @@ static ssize_t reset_subordinate_store(struct device *dev,
 		return -EINVAL;
 
 	if (val) {
-		int ret = pci_try_reset_bus(bus);
+		int ret = pci_try_reset_bridge(pdev);
 
 		if (ret)
 			return ret;
