6a932064b4ee55e0a244831dacf80f5a63358569
Michal Wajdeczko <michal.wajdeczko@intel.com>
2026-02-18 21:55:43 +0100

    drm/xe/pf: Expose LMTT page size
    The underlying LMTT implementation already provides the info about
    the page size it is using.  There is no need to have a separate
    helper function that is making assumption about the required size.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Piotr Piórkowski <piotr.piorkowski@intel.com>
    Reviewed-by: Piotr Piórkowski <piotr.piorkowski@intel.com>
    Link: https://patch.msgid.link/20260218205553.3561-2-michal.wajdeczko@intel.com

--- drivers/gpu/drm/xe/xe_gt_sriov_pf_config.c
+++ drivers/gpu/drm/xe/xe_gt_sriov_pf_config.c
@@ -1301,8 +1301,7 @@ int xe_gt_sriov_pf_config_set_fair_dbs(struct xe_gt *gt, unsigned int vfid,
 
 static u64 pf_get_lmem_alignment(struct xe_gt *gt)
 {
-	/* this might be platform dependent */
-	return SZ_2M;
+	return xe_lmtt_page_size(&gt->tile->sriov.pf.lmtt);
 }
 
 static u64 pf_get_min_spare_lmem(struct xe_gt *gt)
