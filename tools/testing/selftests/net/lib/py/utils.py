31ce56a98004834644456dd12d5edc9fee580b65
Jakub Kicinski <kuba@kernel.org>
2026-02-23 12:26:31 -0800

    selftests: net: py: avoid masking exceptions in bkg() failures
    bkg() failures are currently quite hard to debug and spot.
    Often we have code along the lines of:
    
      with bkg("./cmd_rx_something -p PORT"):
           wait_port_listen(PORT)
           cmd("./cmd_tx_something", host=remote)
    
    When wait_port_listen() fails we don't get to see the exit status
    of bkg(). Even tho very often it's a failure in the bkg() command
    that's actually to blame. Try not to interfere with the bkg()
    command error checking.
    
    With:
    
       with bkg("false", exit_wait=True):
            time.sleep(0.01)  # let the 'false' cmd run
            raise Exception("bla")
    
    Before:
    
      .. stack trace ..
      # Exception| Exception: bla
    
    After:
    
      .. stack trace ..
      # Exception| Exception: bla
      # Exception|
      # Exception| During handling of the above exception, another exception occurred:
      .. stack trace ..
      # Exception| lib.py.utils.CmdExitFailure: Command failed: false
      # Exception| STDOUT: b''
      # Exception| STDERR: b''
    
    Reviewed-by: Petr Machata <petrm@nvidia.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20260223202633.4126087-2-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tools/testing/selftests/net/lib/py/utils.py
+++ tools/testing/selftests/net/lib/py/utils.py
@@ -162,8 +162,11 @@ class bkg(cmd):
         return self
 
     def __exit__(self, ex_type, ex_value, ex_tb):
-        # Force termination on exception
-        terminate = self.terminate or (self._exit_wait and ex_type is not None)
+        terminate = self.terminate
+        # Force termination on exception, but only if bkg() didn't already exit
+        # since forcing termination silences failures with fail=None
+        if self.proc.poll() is None:
+            terminate = terminate or (self._exit_wait and ex_type is not None)
         return self.process(terminate=terminate, fail=self.check_fail)
 
 
