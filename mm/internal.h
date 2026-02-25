031c3ef39bb4b666288c2bc1273f6e7a75491581
Pasha Tatashin <pasha.tatashin@soleen.com>
2026-02-25 17:38:56 -0500

    mm/vmalloc: export clear_vm_uninitialized_flag()
    Patch series "Fix KASAN support for KHO restored vmalloc regions".
    
    When KHO restores a vmalloc area, it maps existing physical pages into a
    newly allocated virtual memory area.  However, because these areas were
    not properly unpoisoned, KASAN would treat any access to the restored
    region as out-of-bounds, as seen in the following trace:
    
    BUG: KASAN: vmalloc-out-of-bounds in kho_test_restore_data.isra.0+0x17b/0x2cd
    Read of size 8 at addr ffffc90000025000 by task swapper/0/1
    [...]
    Call Trace:
    [...]
    kasan_report+0xe8/0x120
    kho_test_restore_data.isra.0+0x17b/0x2cd
    kho_test_init+0x15a/0x1f0
    do_one_initcall+0xd5/0x4b0
    
    The fix involves deferring KASAN's default poisoning by using the
    VM_UNINITIALIZED flag during allocation, manually unpoisoning the memory
    once it is correctly mapped, and then clearing the uninitialized flag
    using a newly exported helper.
    
    This patch (of 2):
    
    Make clear_vm_uninitialized_flag() available to other parts of the kernel
    that need to manage vmalloc areas manually, such as KHO for restoring
    vmallocs.
    
    Link: https://lkml.kernel.org/r/20260225220223.1695350-1-pasha.tatashin@soleen.com
    Link: https://lkml.kernel.org/r/20260225223857.1714801-2-pasha.tatashin@soleen.com
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Acked-by: Pratyush Yadav (Google) <pratyush@kernel.org>
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: "Uladzislau Rezki (Sony)" <urezki@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/internal.h
+++ mm/internal.h
@@ -1338,6 +1338,8 @@ int __must_check vmap_pages_range_noflush(unsigned long addr, unsigned long end,
 }
 #endif
 
+void clear_vm_uninitialized_flag(struct vm_struct *vm);
+
 int __must_check __vmap_pages_range_noflush(unsigned long addr,
 			       unsigned long end, pgprot_t prot,
 			       struct page **pages, unsigned int page_shift);
