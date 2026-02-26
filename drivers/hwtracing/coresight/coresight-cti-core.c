bda019c539e2d12d87c0c6ba91cdeabb40a4cd09
Leo Yan <leo.yan@arm.com>
2026-02-26 09:23:53 +0000

    coresight: cti: Rename cti_active() to cti_is_active()
    Rename cti_active() to cti_is_active() to clarify that it checks whether
    the CTI device is active.
    
    Reviewed-by: Mike Leach <mike.leach@arm.com>
    Signed-off-by: Leo Yan <leo.yan@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Link: https://lore.kernel.org/r/20260226-arm_coresight_cti_refactor_v1-v2-5-b30fada3cfec@arm.com

--- drivers/hwtracing/coresight/coresight-cti-core.c
+++ drivers/hwtracing/coresight/coresight-cti-core.c
@@ -192,7 +192,7 @@ void cti_write_intack(struct device *dev, u32 ackval)
 	guard(raw_spinlock_irqsave)(&drvdata->spinlock);
 
 	/* write if enabled */
-	if (cti_active(config))
+	if (cti_is_active(config))
 		cti_write_single_reg(drvdata, CTIINTACK, ackval);
 }
 
@@ -379,7 +379,7 @@ int cti_channel_trig_op(struct device *dev, enum cti_chan_op op,
 		config->ctiouten[trigger_idx] = reg_value;
 
 	/* write through if enabled */
-	if (cti_active(config))
+	if (cti_is_active(config))
 		cti_write_single_reg(drvdata, reg_offset, reg_value);
 
 	return 0;
@@ -417,7 +417,7 @@ int cti_channel_gate_op(struct device *dev, enum cti_chan_gate_op op,
 	}
 	if (err == 0) {
 		config->ctigate = reg_value;
-		if (cti_active(config))
+		if (cti_is_active(config))
 			cti_write_single_reg(drvdata, CTIGATE, reg_value);
 	}
 
@@ -466,7 +466,7 @@ int cti_channel_setop(struct device *dev, enum cti_chan_set_op op,
 		break;
 	}
 
-	if ((err == 0) && cti_active(config))
+	if ((err == 0) && cti_is_active(config))
 		cti_write_single_reg(drvdata, reg_offset, reg_value);
 
 	return err;
