dbd7b36b1398abc5f7bbd25b3201b97a6b796563
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2026-02-27 09:53:59 +0000

    net: stmmac: make dma_cfg mixed/fixed burst boolean
    struct stmmac_dma_cfg mixed_burst/fixed_burst members are both boolean
    in nature - of_property_read_bool() are used to read these from DT, and
    they are only tested for non-zero values. Use bool to avoid unnecessary
    padding in this structure.
    
    Update dwmac-intel to initialise these using true rather than '1', and
    remove the '0' initialisers as the struct is already zero initialised
    on allocation.
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Link: https://patch.msgid.link/E1vvuXn-0000000AvnX-4A1u@rmk-PC.armlinux.org.uk
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-intel.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-intel.c
@@ -661,8 +661,6 @@ static int intel_mgbe_common_data(struct pci_dev *pdev,
 
 	plat->dma_cfg->pbl = 32;
 	plat->dma_cfg->pblx8 = true;
-	plat->dma_cfg->fixed_burst = 0;
-	plat->dma_cfg->mixed_burst = 0;
 	plat->dma_cfg->aal = 0;
 	plat->dma_cfg->dche = true;
 
@@ -1146,7 +1144,7 @@ static int quark_default_data(struct pci_dev *pdev,
 
 	plat->dma_cfg->pbl = 16;
 	plat->dma_cfg->pblx8 = true;
-	plat->dma_cfg->fixed_burst = 1;
+	plat->dma_cfg->fixed_burst = true;
 	/* AXI (TODO) */
 
 	return 0;
