b5270488ed46c65bd52c96f8d7565d17aaf44f62
Christoph Hellwig <hch@lst.de>
2026-03-02 06:18:10 -0800

    fscrypt: move fscrypt_set_bio_crypt_ctx_bh to buffer.c
    fscrypt_set_bio_crypt_ctx_bh is only used by submit_bh_wbc now.  Move it
    there and merge bh_get_inode_and_lblk_num into it.
    
    Note that this does not add ifdefs for fscrypt as the compiler will
    optimize away the dead code if it is not built in.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20260302141922.370070-6-hch@lst.de
    Signed-off-by: Eric Biggers <ebiggers@kernel.org>

--- include/linux/fscrypt.h
+++ include/linux/fscrypt.h
@@ -834,10 +834,6 @@ void fscrypt_set_bio_crypt_ctx(struct bio *bio,
 			       const struct inode *inode, u64 first_lblk,
 			       gfp_t gfp_mask);
 
-void fscrypt_set_bio_crypt_ctx_bh(struct bio *bio,
-				  const struct buffer_head *first_bh,
-				  gfp_t gfp_mask);
-
 bool fscrypt_mergeable_bio(struct bio *bio, const struct inode *inode,
 			   u64 next_lblk);
 
@@ -855,11 +851,6 @@ static inline bool __fscrypt_inode_uses_inline_crypto(const struct inode *inode)
 static inline void fscrypt_set_bio_crypt_ctx(struct bio *bio,
 					     const struct inode *inode,
 					     u64 first_lblk, gfp_t gfp_mask) { }
-
-static inline void fscrypt_set_bio_crypt_ctx_bh(
-					 struct bio *bio,
-					 const struct buffer_head *first_bh,
-					 gfp_t gfp_mask) { }
 
 static inline bool fscrypt_mergeable_bio(struct bio *bio,
 					 const struct inode *inode,
