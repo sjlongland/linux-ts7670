49ca5871f20bc4fb1b59ce7c7694d5a545f799b6
Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
2026-03-02 15:02:33 -0500

    drm/amd/display: Add MRQ programming for DCN42
    [Why]
    DCN401 didn't have a MRQ present so these fields didn't exist.
    
    They are still present on DCN42 so we need to continue programming
    them like we did on DCN35 or we can block have poor meta requesting
    efficiency which blocks p-state.
    
    [How]
    Add `hubp42_program_requestor` which takes DML21 input and programs
    the registers like DCN35 and prior.
    
    Reviewed-by: Leo Chen <leo.chen@amd.com>
    Signed-off-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Roman Li <roman.li@amd.com>
    Signed-off-by: Chuanyu Tseng <Chuanyu.Tseng@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/hubp/dcn42/dcn42_hubp.h
+++ drivers/gpu/drm/amd/display/dc/hubp/dcn42/dcn42_hubp.h
@@ -48,6 +48,8 @@
 	HUBP_SF(CURSOR0_0_HUBP_3DLUT_ADDRESS_LOW, HUBP_3DLUT_ADDRESS_LOW, mask_sh),\
 	HUBP_SF(CURSOR0_0_HUBP_3DLUT_DLG_PARAM, REFCYC_PER_3DLUT_GROUP, mask_sh)
 
+struct dml2_display_rq_regs;
+
 bool hubp42_construct(
 	struct dcn20_hubp *hubp2,
 	struct dc_context *ctx,
@@ -63,6 +65,10 @@ void hubp42_program_3dlut_fl_crossbar(
 	enum hubp_3dlut_fl_crossbar_bit_slice bit_slice_b);
 
 void hubp42_read_state(struct hubp *hubp);
+
+void hubp42_program_requestor(
+		struct hubp *hubp,
+		struct dml2_display_rq_regs *rq_regs);
 
 void hubp42_setup(
 		struct hubp *hubp,
