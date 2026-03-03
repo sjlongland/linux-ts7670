10ff126c19a9f80be77a0e32b4ad6d14702e8b2a
Steven Rostedt <rostedt@goodmis.org>
2026-03-02 21:35:46 -0500

    ftrace: Disable preemption in the tracepoint callbacks handling filtered pids
    When function trace PID filtering is enabled, the function tracer will
    attach a callback to the fork tracepoint as well as the exit tracepoint
    that will add the forked child PID to the PID filtering list as well as
    remove the PID that is exiting.
    
    Commit a46023d5616e ("tracing: Guard __DECLARE_TRACE() use of
    __DO_TRACE_CALL() with SRCU-fast") removed the disabling of preemption
    when calling tracepoint callbacks.
    
    The callbacks used for the PID filtering accounting depended on preemption
    being disabled, and now the trigger a "suspicious RCU usage" warning message.
    
    Make them explicitly disable preemption.
    
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Link: https://patch.msgid.link/20260302213546.156e3e4f@gandalf.local.home
    Fixes: a46023d5616e ("tracing: Guard __DECLARE_TRACE() use of __DO_TRACE_CALL() with SRCU-fast")
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -8520,6 +8520,7 @@ ftrace_pid_follow_sched_process_fork(void *data,
 	struct trace_pid_list *pid_list;
 	struct trace_array *tr = data;
 
+	guard(preempt)();
 	pid_list = rcu_dereference_sched(tr->function_pids);
 	trace_filter_add_remove_task(pid_list, self, task);
 
@@ -8533,6 +8534,7 @@ ftrace_pid_follow_sched_process_exit(void *data, struct task_struct *task)
 	struct trace_pid_list *pid_list;
 	struct trace_array *tr = data;
 
+	guard(preempt)();
 	pid_list = rcu_dereference_sched(tr->function_pids);
 	trace_filter_add_remove_task(pid_list, NULL, task);
 
