305640652a6ad51dc94379d47fbcf7f5efbf9f53
Xu Yang <xu.yang_2@nxp.com>
2026-03-04 19:39:16 +0800

    usb: dwc3: fix a typo 'HishSpeed'
    It should be 'HighSpeed' instead of 'HishSpeed'.
    
    Signed-off-by: Xu Yang <xu.yang_2@nxp.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@nxp.com>
    Acked-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Link: https://patch.msgid.link/20260304113916.856841-2-xu.yang_2@nxp.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -1135,7 +1135,7 @@ struct dwc3_scratchpad_array {
  *			VBUS with an external supply.
  * @parkmode_disable_ss_quirk: set if we need to disable all SuperSpeed
  *			instances in park mode.
- * @parkmode_disable_hs_quirk: set if we need to disable all HishSpeed
+ * @parkmode_disable_hs_quirk: set if we need to disable all HighSpeed
  *			instances in park mode.
  * @gfladj_refclk_lpm_sel: set if we need to enable SOF/ITP counter
  *                          running based on ref_clk
