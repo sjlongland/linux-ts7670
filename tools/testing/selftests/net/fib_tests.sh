a16dde84b159d10479fe63b3dd70c725355041b9
Alok Tiwari <alok.a.tiwari@oracle.com>
2026-03-06 10:08:19 -0800

    selftests: fib_tests: fix link-local retrieval in fib6_nexthop()
    fib6_nexthop() retrieves the link-local address for two interfaces used
    in the test. However, both lldummy and llv1 are obtained from dummy0.
    
    llv1 is expected to be retrieved from veth1, which is the interface used
    later in the test. The subsequent check and error message also expect
    the address to be retrieved from veth1.
    
    Fix this by retrieving llv1 from veth1.
    
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>
    Link: https://patch.msgid.link/20260306180830.2329477-1-alok.a.tiwari@oracle.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tools/testing/selftests/net/fib_tests.sh
+++ tools/testing/selftests/net/fib_tests.sh
@@ -544,7 +544,7 @@ fib4_nexthop()
 fib6_nexthop()
 {
 	local lldummy=$(get_linklocal dummy0)
-	local llv1=$(get_linklocal dummy0)
+	local llv1=$(get_linklocal veth1)
 
 	if [ -z "$lldummy" ]; then
 		echo "Failed to get linklocal address for dummy0"
