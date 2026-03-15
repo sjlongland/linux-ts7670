f035cdb4d13cf602c46cff052683c0222f82e810
Srinivasan Shanmugam <srinivasan.shanmugam@amd.com>
2026-03-15 18:30:26 +0530

    drm/amd/display: Fix DisplayID not-found handling in parse_edid_displayid_vrr()
    parse_edid_displayid_vrr() searches the EDID extension blocks for a
    DisplayID extension before parsing the dynamic video timing range.
    
    The code previously checked whether edid_ext was NULL after the search
    loop. However, edid_ext is assigned during each iteration of the loop,
    so it will never be NULL once the loop has executed. If no DisplayID
    extension is found, edid_ext ends up pointing to the last extension
    block, and the NULL check does not correctly detect the failure case.
    
    Instead, check whether the loop completed without finding a matching
    DisplayID block by testing "i == edid->extensions". This ensures the
    function exits early when no DisplayID extension is present and avoids
    parsing an unrelated EDID extension block.
    
    Also simplify the EDID validation check using "!edid ||
    !edid->extensions".
    
    Fixes the below:
    drivers/gpu/drm/amd/amdgpu/../display/amdgpu_dm/amdgpu_dm.c:13079 parse_edid_displayid_vrr() warn: variable dereferenced before check 'edid_ext' (see line 13075)
    
    Fixes: a638b837d0e6 ("drm/amd/display: Fix refresh rate range for some panel")
    Cc: Roman Li <roman.li@amd.com>
    Cc: Alex Hung <alex.hung@amd.com>
    Cc: Jerry Zuo <jerry.zuo@amd.com>
    Cc: Sun peng Li <sunpeng.li@amd.com>
    Cc: Tom Chung <chiahsuan.chung@amd.com>
    Cc: Dan Carpenter <dan.carpenter@linaro.org>
    Cc: Aurabindo Pillai <aurabindo.pillai@amd.com>
    Signed-off-by: Srinivasan Shanmugam <srinivasan.shanmugam@amd.com>
    Reviewed-by: Tom Chung <chiahsuan.chung@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -12981,7 +12981,7 @@ static void parse_edid_displayid_vrr(struct drm_connector *connector,
 	u16 min_vfreq;
 	u16 max_vfreq;
 
-	if (edid == NULL || edid->extensions == 0)
+	if (!edid || !edid->extensions)
 		return;
 
 	/* Find DisplayID extension */
@@ -12991,7 +12991,7 @@ static void parse_edid_displayid_vrr(struct drm_connector *connector,
 			break;
 	}
 
-	if (edid_ext == NULL)
+	if (i == edid->extensions)
 		return;
 
 	while (j < EDID_LENGTH) {
