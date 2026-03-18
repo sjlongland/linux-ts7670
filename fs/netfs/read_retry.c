81c3b1c674e39d564096eaf985f10a5cd98af448
David Howells <dhowells@redhat.com>
2026-03-18 15:38:58 +0000

    netfs: Fix read abandonment during retry
    Under certain circumstances, all the remaining subrequests from a read
    request will get abandoned during retry.  The abandonment process expects
    the 'subreq' variable to be set to the place to start abandonment from, but
    it doesn't always have a useful value (it will be uninitialised on the
    first pass through the loop and it may point to a deleted subrequest on
    later passes).
    
    Fix the first jump to "abandon:" to set subreq to the start of the first
    subrequest expected to need retry (which, in this abandonment case, turned
    out unexpectedly to no longer have NEED_RETRY set).
    
    Also clear the subreq pointer after discarding superfluous retryable
    subrequests to cause an oops if we do try to access it.
    
    Fixes: ee4cdf7ba857 ("netfs: Speed up buffered reading")
    Signed-off-by: David Howells <dhowells@redhat.com>
    Link: https://patch.msgid.link/3775287.1773848338@warthog.procyon.org.uk
    Reviewed-by: Paulo Alcantara (Red Hat) <pc@manguebit.org>
    cc: Paulo Alcantara <pc@manguebit.org>
    cc: netfs@lists.linux.dev
    cc: linux-fsdevel@vger.kernel.org
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/netfs/read_retry.c
+++ fs/netfs/read_retry.c
@@ -92,8 +92,10 @@ static void netfs_retry_read_subrequests(struct netfs_io_request *rreq)
 		       from->start, from->transferred, from->len);
 
 		if (test_bit(NETFS_SREQ_FAILED, &from->flags) ||
-		    !test_bit(NETFS_SREQ_NEED_RETRY, &from->flags))
+		    !test_bit(NETFS_SREQ_NEED_RETRY, &from->flags)) {
+			subreq = from;
 			goto abandon;
+		}
 
 		list_for_each_continue(next, &stream->subrequests) {
 			subreq = list_entry(next, struct netfs_io_subrequest, rreq_link);
@@ -177,6 +179,7 @@ static void netfs_retry_read_subrequests(struct netfs_io_request *rreq)
 				if (subreq == to)
 					break;
 			}
+			subreq = NULL;
 			continue;
 		}
 
