046b5fb2a70edb3e8709ce75734fe4a1958cc553
Erikas Bitovtas <xerikasxx@gmail.com>
2026-03-20 18:45:40 +0200

    iio: light: vcnl4000: remove error messages for trigger and irq
    The error code is available in the log after return. In our case,
    attaching a triggered buffer can only fail if we are out of memory, as
    no other buffer is being attached. Remove duplicate error messages to
    reduce noise in dmesg.
    
    Reviewed-by: David Lechner <dlechner@baylibre.com>
    Signed-off-by: Erikas Bitovtas <xerikasxx@gmail.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@intel.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/light/vcnl4000.c
+++ drivers/iio/light/vcnl4000.c
@@ -2047,11 +2047,8 @@ static int vcnl4000_probe(struct i2c_client *client)
 						      NULL,
 						      data->chip_spec->trig_buffer_func,
 						      data->chip_spec->buffer_setup_ops);
-		if (ret < 0) {
-			dev_err(&client->dev,
-				"unable to setup iio triggered buffer\n");
+		if (ret < 0)
 			return ret;
-		}
 	}
 
 	if (client->irq && data->chip_spec->irq_thread) {
@@ -2061,10 +2058,8 @@ static int vcnl4000_probe(struct i2c_client *client)
 						IRQF_ONESHOT,
 						"vcnl4000_irq",
 						indio_dev);
-		if (ret < 0) {
-			dev_err(&client->dev, "irq request failed\n");
+		if (ret < 0)
 			return ret;
-		}
 
 		ret = vcnl4010_probe_trigger(indio_dev);
 		if (ret < 0)
