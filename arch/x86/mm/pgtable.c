7df5ebca6fb62f9116dc2acb9412aaf755cfc8a8
Baolin Wang <baolin.wang@linux.alibaba.com>
2026-03-21 14:42:52 +0800

    mm: change to return bool for pmdp_clear_flush_young()
    The pmdp_clear_flush_young() is used to clear the young flag and flush the
    TLB, returning whether the young flag was set for this PMD entry.  Change
    the return type to bool to make the intention clearer.
    
    Link: https://lkml.kernel.org/r/a668b9a974c0d675e7a41f6973bcbe3336e8b373.1774075004.git.baolin.wang@linux.alibaba.com
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Lorenzo Stoakes (Oracle) <ljs@kernel.org>
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Ritesh Harjani (IBM) <ritesh.list@gmail.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/x86/mm/pgtable.c
+++ arch/x86/mm/pgtable.c
@@ -573,10 +573,10 @@ int ptep_clear_flush_young(struct vm_area_struct *vma,
 }
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
-int pmdp_clear_flush_young(struct vm_area_struct *vma,
-			   unsigned long address, pmd_t *pmdp)
+bool pmdp_clear_flush_young(struct vm_area_struct *vma,
+		unsigned long address, pmd_t *pmdp)
 {
-	int young;
+	bool young;
 
 	VM_BUG_ON(address & ~HPAGE_PMD_MASK);
 
