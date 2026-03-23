f68de74588bcdcbd738393cbd6e96e359069f8a4
Srinivasan Shanmugam <srinivasan.shanmugam@amd.com>
2026-03-23 10:08:57 +0530

    drm/amd/display: Add update_descriptor param info in 'update_planes_and_stream_state'
    Add missing info for the update_descriptor parameter in
    update_planes_and_stream_state().
    
    Fixes the below with gcc W=1:
    ../display/dc/core/dc.c:3630 function parameter 'update_descriptor' not described in 'update_planes_and_stream_state'
    
    Fixes: c24bb00cc6cf ("drm/amd/display: Refactor DC update checks")
    Cc: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Cc: Dillon Varone <Dillon.Varone@amd.com>
    Cc: Chuanyu Tseng <chuanyu.tseng@amd.com>
    Cc: Roman Li <roman.li@amd.com>
    Cc: Alex Hung <alex.hung@amd.com>
    Cc: Tom Chung <chiahsuan.chung@amd.com>
    Cc: Dan Carpenter <dan.carpenter@linaro.org>
    Cc: Aurabindo Pillai <aurabindo.pillai@amd.com>
    Signed-off-by: Srinivasan Shanmugam <srinivasan.shanmugam@amd.com>
    Reviewed-by: Alex Hung <alex.hung@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc.c
+++ drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -3352,6 +3352,7 @@ static void update_seamless_boot_flags(struct dc *dc,
  * @surface_count: surface update count
  * @stream: Corresponding stream to be updated
  * @stream_update: stream update
+ * @update_descriptor: describes what plane and stream changes to apply
  * @new_update_type: [out] determined update type by the function
  * @new_context: [out] new context allocated and validated if update type is
  * FULL, reference to current context if update type is less than FULL.
