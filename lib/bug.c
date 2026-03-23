8713d229eb0aa8d9d23f4ac53d28924c0c8c1f73
Arnd Bergmann <arnd@arndb.de>
2026-03-23 21:55:16 +0100

    bug: avoid format attribute warning for clang as well
    Like gcc, clang-22 now also warns about a function that it incorrectly
    identifies as a printf-style format:
    
    lib/bug.c:190:22: error: diagnostic behavior may be improved by adding the 'format(printf, 1, 0)' attribute to the declaration of '__warn_printf' [-Werror,-Wmissing-format-attribute]
      179 | static void __warn_printf(const char *fmt, struct pt_regs *regs)
          | __attribute__((format(printf, 1, 0)))
      180 | {
      181 |         if (!fmt)
      182 |                 return;
      183 |
      184 | #ifdef HAVE_ARCH_BUG_FORMAT_ARGS
      185 |         if (regs) {
      186 |                 struct arch_va_list _args;
      187 |                 va_list *args = __warn_args(&_args, regs);
      188 |
      189 |                 if (args) {
      190 |                         vprintk(fmt, *args);
          |                                           ^
    
    Revert the change that added a gcc-specific workaround, and instead add
    the generic annotation that avoid the warning.
    
    Link: https://lkml.kernel.org/r/20260323205534.1284284-1-arnd@kernel.org
    Fixes: d36067d6ea00 ("bug: Hush suggest-attribute=format for __warn_printf()")
    Suggested-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Suggested-by: Brendan Jackman <jackmanb@google.com>
    Link: https://lore.kernel.org/all/20251208141618.2805983-1-andriy.shevchenko@linux.intel.com/T/#u
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Brendan Jackman <jackmanb@google.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Bill Wendling <morbo@google.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Justin Stitt <justinstitt@google.com>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/bug.c
+++ lib/bug.c
@@ -163,10 +163,8 @@ struct bug_entry *find_bug(unsigned long bugaddr)
 	return module_find_bug(bugaddr);
 }
 
-__diag_push();
-__diag_ignore(GCC, all, "-Wsuggest-attribute=format",
-	      "Not a valid __printf() conversion candidate.");
-static void __warn_printf(const char *fmt, struct pt_regs *regs)
+static __printf(1, 0)
+void __warn_printf(const char *fmt, struct pt_regs *regs)
 {
 	if (!fmt)
 		return;
@@ -185,7 +183,6 @@ static void __warn_printf(const char *fmt, struct pt_regs *regs)
 
 	printk("%s", fmt);
 }
-__diag_pop();
 
 static enum bug_trap_type __report_bug(struct bug_entry *bug, unsigned long bugaddr, struct pt_regs *regs)
 {
