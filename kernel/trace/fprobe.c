1e5f8e4cee4336caac027bc7b8772a99a160edf5
Martin Kaiser <martin@kaiser.cx>
2026-03-25 00:47:01 +0900

    tracing: fprobe: do not zero out unused fgraph_data
    If fprobe_entry does not fill the allocated fgraph_data completely, the
    unused part does not have to be zeroed.
    
    fgraph_data is a short-lived part of the shadow stack. The preceding
    length field allows locating the end regardless of the content.
    
    Link: https://lore.kernel.org/all/20260324084804.375764-1-martin@kaiser.cx/
    
    Signed-off-by: Martin Kaiser <martin@kaiser.cx>
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>

--- kernel/trace/fprobe.c
+++ kernel/trace/fprobe.c
@@ -328,8 +328,6 @@ static int fprobe_entry(struct ftrace_graph_ent *trace, struct fgraph_ops *gops,
 				used += FPROBE_HEADER_SIZE_IN_LONG + size_words;
 		}
 	}
-	if (used < reserved_words)
-		memset(fgraph_data + used, 0, reserved_words - used);
 
 	/* If any exit_handler is set, data must be used. */
 	return used != 0;
