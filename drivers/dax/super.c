be3650a08d20aad45bde070750f340e39d4ef18f
John Groves <john@groves.net>
2026-03-27 21:05:21 +0000

    dax: export dax_dev_get()
    famfs needs to look up a dax_device by dev_t when resolving fmap
    entries that reference character dax devices.
    
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Signed-off-by: John Groves <john@groves.net>
    Link: https://patch.msgid.link/0100019d311daab5-bb212f0b-4e05-4668-bf53-d76fab56be68-000000@email.amazonses.com
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- drivers/dax/super.c
+++ drivers/dax/super.c
@@ -422,7 +422,7 @@ static int dax_set(struct inode *inode, void *data)
 	return 0;
 }
 
-static struct dax_device *dax_dev_get(dev_t devt)
+struct dax_device *dax_dev_get(dev_t devt)
 {
 	struct dax_device *dax_dev;
 	struct inode *inode;
@@ -445,6 +445,7 @@ static struct dax_device *dax_dev_get(dev_t devt)
 
 	return dax_dev;
 }
+EXPORT_SYMBOL_GPL(dax_dev_get);
 
 struct dax_device *alloc_dax(void *private, const struct dax_operations *ops)
 {
