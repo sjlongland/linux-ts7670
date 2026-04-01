f16186a8a4ebef3e832faccc63714a8b746a6db5
Aravind Anilraj <aravindanilraj0702@gmail.com>
2026-04-01 18:05:06 -0400

    ASoC: Intel: cht_bsw_rt5672: Fix MCLK leak on platform_clock_control error
    If snd_soc_dai_set_pll() or snd_soc_dai_set_sysclk() fail inside the
    EVENT_ON path, the function returns without calling
    clk_disable_unprepare() on ctx->mclk, which was already enabled earlier
    in the same code path. Add the missing clk_disable_unprepare() calls
    before returning the error.
    
    Signed-off-by: Aravind Anilraj <aravindanilraj0702@gmail.com>
    Reviewed-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Link: https://patch.msgid.link/20260401220507.23557-4-aravindanilraj0702@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/boards/cht_bsw_rt5672.c
+++ sound/soc/intel/boards/cht_bsw_rt5672.c
@@ -76,6 +76,8 @@ static int platform_clock_control(struct snd_soc_dapm_widget *w,
 				CHT_PLAT_CLK_3_HZ, 48000 * 512);
 		if (ret < 0) {
 			dev_err(card->dev, "can't set codec pll: %d\n", ret);
+			if (ctx->mclk)
+				clk_disable_unprepare(ctx->mclk);
 			return ret;
 		}
 
@@ -84,6 +86,8 @@ static int platform_clock_control(struct snd_soc_dapm_widget *w,
 			48000 * 512, SND_SOC_CLOCK_IN);
 		if (ret < 0) {
 			dev_err(card->dev, "can't set codec sysclk: %d\n", ret);
+			if (ctx->mclk)
+				clk_disable_unprepare(ctx->mclk);
 			return ret;
 		}
 	} else {
