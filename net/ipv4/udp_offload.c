011225bbf13cda8480bace634f8af78ac9c4cc00
Eric Dumazet <edumazet@google.com>
2026-04-02 17:44:30 +0000

    inet: remove leftover EXPORT_SYMBOL()
    IPv6 is no longer a module, we no longer need to export these symbols.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Fernando Fernandez Mancera <fmancera@suse.de>
    Link: https://patch.msgid.link/20260402174430.2462800-1-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/ipv4/udp_offload.c
+++ net/ipv4/udp_offload.c
@@ -187,7 +187,6 @@ out_unlock:
 
 	return segs;
 }
-EXPORT_SYMBOL(skb_udp_tunnel_segment);
 
 static void __udpv4_gso_segment_csum(struct sk_buff *seg,
 				     __be32 *oldip, __be32 *newip,
@@ -412,7 +411,6 @@ struct sk_buff *__udp_gso_segment(struct sk_buff *gso_skb,
 	}
 	return segs;
 }
-EXPORT_SYMBOL_GPL(__udp_gso_segment);
 
 static struct sk_buff *udp4_ufo_fragment(struct sk_buff *skb,
 					 netdev_features_t features)
@@ -628,7 +626,6 @@ out:
 	skb_gro_flush_final(skb, pp, flush);
 	return pp;
 }
-EXPORT_SYMBOL(udp_gro_receive);
 
 static struct sock *udp4_gro_lookup_skb(struct sk_buff *skb, __be16 sport,
 					__be16 dport)
@@ -729,7 +726,6 @@ int udp_gro_complete(struct sk_buff *skb, int nhoff,
 
 	return err;
 }
-EXPORT_SYMBOL(udp_gro_complete);
 
 INDIRECT_CALLABLE_SCOPE int udp4_gro_complete(struct sk_buff *skb, int nhoff)
 {
