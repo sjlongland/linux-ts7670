b820af6580c046fa07c89dd7d8d658d0f5dcbb3c
Johan Hovold <johan@kernel.org>
2026-04-09 14:04:18 +0200

    spi: microchip-core-spi: fix controller deregistration
    Make sure to deregister the controller before disabling underlying
    resources like interrupts during driver unbind.
    
    Fixes: 059f545832be ("spi: add support for microchip "soft" spi controller")
    Cc: stable@vger.kernel.org	# 6.19
    Cc: Prajna Rajendra Kumar <prajna.rajendrakumar@microchip.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Acked-by: Conor Dooley <conor.dooley@microchip.com>
    Link: https://patch.msgid.link/20260409120419.388546-20-johan@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-microchip-core-spi.c
+++ drivers/spi/spi-microchip-core-spi.c
@@ -385,7 +385,7 @@ static int mchp_corespi_probe(struct platform_device *pdev)
 
 	mchp_corespi_init(host, spi);
 
-	ret = devm_spi_register_controller(dev, host);
+	ret = spi_register_controller(host);
 	if (ret) {
 		mchp_corespi_disable_ints(spi);
 		mchp_corespi_disable(spi);
@@ -399,6 +399,8 @@ static void mchp_corespi_remove(struct platform_device *pdev)
 {
 	struct spi_controller *host = platform_get_drvdata(pdev);
 	struct mchp_corespi *spi = spi_controller_get_devdata(host);
+
+	spi_unregister_controller(host);
 
 	mchp_corespi_disable_ints(spi);
 	mchp_corespi_disable(spi);
