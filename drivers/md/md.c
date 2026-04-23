d8114d89346049fd32690dcfde7837cf9001c3c5
Abd-Alrhman Masalkhi <abd.masalkhi@gmail.com>
2026-04-23 12:13:02 +0200

    md: use mddev_is_dm() instead of open-coding gendisk checks
    Replace direct checks on mddev->gendisk with mddev_is_dm() in
    md_handle_request() and md_run().
    
    Signed-off-by: Abd-Alrhman Masalkhi <abd.masalkhi@gmail.com>
    Link: https://lore.kernel.org/linux-raid/20260423101303.48196-3-abd.masalkhi@gmail.com
    Signed-off-by: Yu Kuai <yukuai@fnnas.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -385,7 +385,7 @@ check_suspended:
 
 	if (!mddev->pers->make_request(mddev, bio)) {
 		percpu_ref_put(&mddev->active_io);
-		if (!mddev->gendisk && mddev->pers->prepare_suspend)
+		if (mddev_is_dm(mddev) && mddev->pers->prepare_suspend)
 			return false;
 		goto check_suspended;
 	}
@@ -6252,7 +6252,7 @@ int md_run(struct mddev *mddev)
 	}
 
 	/* dm-raid expect sync_thread to be frozen until resume */
-	if (mddev->gendisk)
+	if (!mddev_is_dm(mddev))
 		mddev->recovery = 0;
 
 	/* may be over-ridden by personality */
